be4c883af72e4840c09b4d451d4b67c9ee79e817
Cas S <ezgraphs@gmail.com>
2011-11-15 06:48:37 -0500

    Changes to retrieve abstract and  image during parse.  Slow - expected runtime over 24 hours

--- gamespot_com/parse.rb
+++ gamespot_com/parse.rb
@@ -2,7 +2,7 @@
 ['rubygems', 'hpricot', 'open-uri'].each{|r|require r}
 
 DOWNLOAD_DIR='download'
-
+PARSE_DOWNLOAD_DIR='parse_download'
 # Have logic related to previous record (to aggregate game systems for a game)
 @game_system      = []
 @external_links   = []
@@ -12,10 +12,29 @@ DOWNLOAD_DIR='download'
 @page             = ''
 @source_url       = ''
 
+def get_details(url,name)
+  img=''
+  abstract_text=''
+  
+  begin   
+     output = "#{PARSE_DOWNLOAD_DIR}/" + name.gsub(/(\.|\-|\:|\'| )/,'_')+".html"
+     system "curl -L #{url} -o #{output}" 
+     doc2=Hpricot(open(output))
+     abstract_text = (doc2/"//span[@class='mainDeck']").inner_text
+     img=(doc2/"//div[@class='boxshot']/a/img").first['src']     
+  rescue
+     puts "*** WARNING: Could not retrieve abstract / image for #{url}"
+  end
+  
+  return [img,abstract_text]
+end
+
 def parse_file(file, output_file)
    
+   system "mkdir -p #{PARSE_DOWNLOAD_DIR}"
    doc=Hpricot(open(file))
    apps = doc/"//a"
+
    apps.each do |l|
 
    internal_links  = ''
@@ -26,7 +45,9 @@ def parse_file(file, output_file)
    if (not @prev_page.nil?) and tmp[0].strip.downcase != @prev_page.downcase 
       external_links='' # Aggregate the external links into a string
       @external_links.each_with_index{|link,i|external_links+="[#{link} #{@game_system[i]}]\\n"}
-       
+      
+      # Go get the detail information once we are down to one version of the game...
+      @images,@text_abstract=get_details(@source_url, @page) 
       str="#{@page}\tA\t\t\t#{categories}\t\t#{internal_links}\t\t#{external_links}\t\t#{@images}\t#{@text_abstract}\t#{@source_url}\n"
       File.open(output_file, 'a'){|f|f.puts str}
      
