ca183eccebc5416af4670e7a5f5089f86e586b3c
Cas S <ezgraphs@gmail.com>
2011-11-21 11:13:38 -0500

    Updates to fetch and parse (multiprocess)

--- gamespot_com/parse.rb
+++ gamespot_com/parse.rb
@@ -2,7 +2,7 @@
 ['rubygems', 'hpricot', 'open-uri'].each{|r|require r}
 
 DOWNLOAD_DIR='download'
-PARSE_DOWNLOAD_DIR='parse_download'
+
 # Have logic related to previous record (to aggregate game systems for a game)
 @game_system      = []
 @external_links   = []
@@ -12,26 +12,26 @@ PARSE_DOWNLOAD_DIR='parse_download'
 @page             = ''
 @source_url       = ''
 
-def get_details(url,name)
+def get_details(url,name, output_file)
   img=''
   abstract_text=''
   
   begin   
-     output = "#{PARSE_DOWNLOAD_DIR}/" + name.gsub(/(\.|\-|\:|\'| )/,'_')+".html"
-     system "curl -L #{url} -o #{output}" 
-     doc2=Hpricot(open(output))
+     tmp="tmp_"+output_file
+     system "curl -L #{url} -o #{tmp}" 
+     doc2=Hpricot(open(tmp))
      abstract_text = (doc2/"//span[@class='mainDeck']").inner_text
      img=(doc2/"//div[@class='boxshot']/a/img").first['src']     
   rescue
      puts "*** WARNING: Could not retrieve abstract / image for #{url}"
   end
-  
+
+  system "rm #{tmp}" rescue puts "WARNING: Could not remove #{tmp} (#{$!})"
   return [img,abstract_text]
 end
 
 def parse_file(file, output_file)
    
-   system "mkdir -p #{PARSE_DOWNLOAD_DIR}"
    doc=Hpricot(open(file))
    apps = doc/"//a"
 
@@ -47,7 +47,7 @@ def parse_file(file, output_file)
       @external_links.each_with_index{|link,i|external_links+="[#{link} #{@game_system[i]}]\\n"}
       
       # Go get the detail information once we are down to one version of the game...
-      @images,@text_abstract=get_details(@source_url, @page) 
+      @images,@text_abstract=get_details(@source_url, @page, output_file) 
       str="#{@page}\tA\t\t\t#{categories}\t\t#{internal_links}\t\t#{external_links}\t\t#{@images}\t#{@text_abstract}\t#{@source_url}\n"
       File.open(output_file, 'a'){|f|f.puts str}
      
