477c7ad5c891f5f3359dbb25cbd5a5b848ab5726
eet-fuk <dutch109@gmail.com>
2011-12-02 23:42:26 +0100

    Ignore options without help text + handle options with multiple descriptions + more verbose script output

--- lkddb/parse.py
+++ lkddb/parse.py
@@ -23,14 +23,11 @@ class KernelConfigItem:
 
   def __str__(self):
     redirect = "%s\tR\t%s\t\t\t\t\t\t\t\t\t\t" % (self.name.split("_",1)[1],self.name)
-    # TODO format the snippet better
     if self.help:
       snippet = self.help
-    else:
-      snippet = ""
     """
     if self.type:
-      snippet = "%s (%s)" % (snippet, self.type)
+      snippet = "%s\\n- type: %s" % (snippet, self.type)
     if self.depends:
       snippet = "%s\\n- depends on the following option(s): %s" % (snippet, self.depends)
     if self.defined:
@@ -39,8 +36,6 @@ class KernelConfigItem:
       snippet = "%s\\n- available in the following Linux version(s): %s" % (snippet, self.kernelversions)
     if self.modules:
       snippet = "%s\\n- will build the following module(s): %s" % (snippet, self.modules)
-    if snippet.startswith("\\n"):
-      snippet = snippet[2:]
     """
     fields = [ self.name,
                "A",
@@ -77,68 +72,80 @@ class LkddbScraper:
 
   def __next__(self):
     logger = logging.getLogger()
+
     # get subpages
     for sub_page_tag in self.main_page_xml.iterfind("body/ul/li/a"):
       sub_page_url = "%s%s" % (self.BASE_URL, sub_page_tag.attrib["href"])
       sub_page = self.get_page_from_cache(sub_page_url)
       sub_page_xml = lxml.etree.XML(sub_page,self.parser)
+
       # get config item page
       for config_page_tag in sub_page_xml.iterfind("body/ul/li/ul/li/a"):
         config_page_url = "%s%s" % (self.BASE_URL, config_page_tag.attrib["href"])
         config_page = self.get_page_from_cache(config_page_url)
         config_page_xml = lxml.etree.XML(config_page,self.parser)
+
         try:
+          # has this option several descriptions?
           multiple = len(config_page_xml.findall("body/div/ul")) > 3
+
           # get name of config option
           name = config_page_xml.find("body/div/h1").text
           if name.find(":") != -1:
             name = name.split(":",1)[0]
-          if not multiple:
-            # get full help
-            help_lines = []
-            for help_line in config_page_xml.xpath("body/div/h2[text()='Help text']/following-sibling::*"):
-              if help_line.tag != "p":
-                break
-              help_lines.append(lxml.etree.tostring(help_line,encoding="unicode",method="text").replace("\n"," ").strip())
-            help = "\\n".join(help_lines)
-            if help == "(none)":
-              help = ""
-            # detect erroneous pages
-            pre_list_msg_xml = config_page_xml.xpath("body/div/h2[text()='General informations']/following::p")
-            if not pre_list_msg_xml:
-              # for some pages lxml fail to get the right xml structure
-              # eg: http://cateee.net/lkddb/web-lkddb/M25PXX_USE_FAST_READ.html (xpath query "body/div/h2[text()='General informations']" gives nothing, but "body//h2[text()='General informations']" does)
-              self.ko_count += 1
-              logger.warning("Parsing of page '%s' failed (invalid xml structure)" % (config_page_url) )
-              continue
-            pre_list_msg_xml = pre_list_msg_xml[0]
-            pre_list_msg = lxml.etree.tostring(pre_list_msg_xml,encoding="unicode",method="text").strip()
-            if pre_list_msg.endswith("error: definition not found!"):
-              # some pages are incomplete, eg: http://cateee.net/lkddb/web-lkddb/ATHEROS_AR71XX.html
-              self.ko_count += 1
-              logger.warning("Parsing of page '%s' failed (incomplete page)" % (config_page_url) )
-              continue
-            # get other option info
-            li_list = list(x.text for x in config_page_xml.xpath("body/div/h2[text()='General informations']/following::ul/li") if x.text)
-            shorthelp, type, depends, defined, kernelversions, modules = None, None, None, None, None, None
-            for li in li_list:
-              if li.startswith("prompt: "):
-                shorthelp = li[8:]
-              elif li.startswith("type: "):
-                type = li[6:]
-              elif li.startswith("depends on: "):
-                depends = li[12:]
-              elif li.startswith("defined in: "):
-                defined = li[12:]
-              elif li.startswith("found in Linux kernels: "):
-                kernelversions = li[24:]
-              elif li.startswith("modules built: "):
-                modules = li[15:]
-          else:
-            # TODO handle options with more than one description (lots!)
+
+          # get full help
+          help_lines = []
+          for help_line in config_page_xml.xpath("body/div/*[self::h2 or self::h3][text()='Help text']/following-sibling::*"):
+            if help_line.tag != "p":
+              break
+            help_lines.append(lxml.etree.tostring(help_line,encoding="unicode",method="text").replace("\n"," ").strip())
+          help = "\\n".join(help_lines)
+          if (not help) or (help == "(none)"):
+            self.ko_count += 1
+            logger.warning("Skipping page '%s' (not enough content)" % (config_page_url) )
             continue
+
+          # detect erroneous pages
+          pre_list_msg_xml = config_page_xml.xpath("body/div/h2[text()='General informations']/following::p")
+          if not config_page_xml.xpath("body/div/h2[text()='General informations']/following::p"):
+            # for some pages lxml fail to get the right xml structure
+            # eg: http://cateee.net/lkddb/web-lkddb/M25PXX_USE_FAST_READ.html (xpath query "body/div/h2[text()='General informations']" gives nothing, but "body//h2[text()='General informations']" does)
+            self.ko_count += 1
+            logger.warning("Parsing of page '%s' failed (invalid xml structure)" % (config_page_url) )
+            continue
+          pre_list_msg_xml = pre_list_msg_xml[0]
+          pre_list_msg = lxml.etree.tostring(pre_list_msg_xml,encoding="unicode",method="text").strip()
+          if pre_list_msg.endswith("error: definition not found!"):
+            # some pages are incomplete, eg: http://cateee.net/lkddb/web-lkddb/ATHEROS_AR71XX.html
+            self.ko_count += 1
+            logger.warning("Parsing of page '%s' failed (incomplete page)" % (config_page_url) )
+            continue
+
+          # get other option info
+          if multiple:
+            li_list = list(x.text for x in config_page_xml.xpath("body/div/h2[1]/following::ul/li") if x.text)
+          else:
+            li_list = list(x.text for x in config_page_xml.xpath("body/div/h2[text()='General informations']/following::ul/li") if x.text)
+          shorthelp, type, depends, defined, kernelversions, modules = None, None, None, None, None, None
+          for li in li_list:
+            if li.startswith("prompt: "):
+              shorthelp = li[8:]
+            elif li.startswith("type: "):
+              type = li[6:]
+            elif li.startswith("depends on: "):
+              depends = li[12:]
+            elif li.startswith("defined in: "):
+              defined = li[12:]
+            elif li.startswith("found in Linux kernels: "):
+              kernelversions = li[24:]
+            elif li.startswith("modules built: "):
+              modules = li[15:]
+
           self.ok_count += 1
+          logger.info("Page '%s' parsed successfully" % (config_page_url) )
           yield KernelConfigItem(config_page_url,name,shorthelp,help,type,depends,defined,kernelversions,modules)
+
         except UnicodeDecodeError:
           # some pages contain badly encoded chars, eg: http://cateee.net/lkddb/web-lkddb/SA1100_PFS168.html
           self.ko_count += 1
@@ -169,12 +176,12 @@ if __name__ == '__main__':
 
   # setup logger
   logger = logging.getLogger()
-  logger.setLevel(logging.WARNING)
+  logger.setLevel(logging.INFO)
   
   # dump config items
   scraper = LkddbScraper()
   with open("output.txt","wt") as output_file:
     for config_item in scraper:
       output_file.write(str(config_item))
-  logger.info("%d config items parsed successfully\n%d parsing errors" % (scraper.ok_count, scraper.ko_count) )
+  logger.warning("%d config items parsed successfully\n%d skipped pages" % (scraper.ok_count, scraper.ko_count) )
 
