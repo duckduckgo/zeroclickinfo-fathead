7738fc5f1345bb9e5f72cedd4f69afbc01a8f000
eet-fuk <dutch109@gmail.com>
2011-12-03 20:33:42 +0100

    Updated scripts to use a more recent static dump

--- lkddb/fetch.sh
+++ lkddb/fetch.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# set this to true if you only wish to test the scripts, to save time and server bandwith
+# set this to true if you only wish to test the scripts, to save time and server bandwith (you need the 7z command line utility)
 USE_STATIC_DUMP=false
 
 cd $(dirname -- "$0")
 
-if $USE_STATIC_DUMP
+if $USE_STATIC_DUMP && [ $(which 7z) ]
 then
-  wget -nc 'ftp://ks384172.kimsufi.com/lkddb-dump-2011-10-30.7z'
-  7z x lkddb-dump-2011-10-30.7z 
+  wget -nc 'ftp://ks384172.kimsufi.com/lkddb-dump-2011-12-03.7z'
+  7z x lkddb-dump-2011-12-03.7z 
 else
   wget -P download -nv -r -np -nc -l 2 -w 0.1 --random-wait 'http://cateee.net/lkddb/web-lkddb/index.html'
 fi
-
