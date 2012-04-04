eff98d377b71bfe270a7beeb002a83ce127dde5a
Ben Boyter <bboyte01@gmail.com>
2012-04-05 08:31:07 +1000

    Include the java processor used by searchco.de

---
+++ java/fetch.sh
@@ -0,0 +1,3 @@
+# Probably best to go to the below and accept otherwise this probably will not work
+# http://www.oracle.com/technetwork/java/javase/downloads/jdk-6u25-doc-download-355137.html
+wget http://download.oracle.com/otn-pub/java/jdk/6u30-b12/jdk-6u30-apidocs.zip
