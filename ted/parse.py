b0a9f7e2909cbe0d889008fd7eea3e896dea886b
Jag Talon <talon.jag@gmail.com>
2012-05-02 09:51:01 +0000

    Followed the format.

--- ted/parse.py
+++ ted/parse.py
@@ -1,6 +1,17 @@
 import csv
+import urllib
 
 ted = csv.reader(open('download/ted.csv', 'rb'))
 talks = [talk for talk in ted]
 for talk in talks[1:]:
-    print talk
+    title = talk[4]
+    article = 'A'
+    categories = talk[6]
+    see_also = "[" + talk[3] + "]"
+    speaker = talk[3].lower()
+    external_links = "[" + talk[0] + talk[1] + " Official site]\\n" + \
+                      "[http://www.ted.com/speakers/" + (speaker).replace(" ", "_") + \
+                      " TED Profile]\\n"
+    abstract = talk[5]
+    print "{title}\t{article}\t\t\t{categories}\t\t{see_also}\t\t{external_links}\t{abstract}\n".format(
+    title=title, article=article, categories=categories, see_also=see_also, external_links=external_links, abstract=abstract)
