3b6d2cd329b1a29a940aa535b76fa3120d83a992
Jag Talon <talon.jag@gmail.com>
2012-05-02 09:53:28 +0000

    Forgot to add .html

--- ted/parse.py
+++ ted/parse.py
@@ -11,7 +11,7 @@ for talk in talks[1:]:
     speaker = talk[3].lower()
     external_links = "[" + talk[0] + talk[1] + " Official site]\\n" + \
                       "[http://www.ted.com/speakers/" + (speaker).replace(" ", "_") + \
-                      " TED Profile]\\n"
+                      ".html TED Profile]\\n"
     abstract = talk[5]
     print "{title}\t{article}\t\t\t{categories}\t\t{see_also}\t\t{external_links}\t{abstract}\n".format(
     title=title, article=article, categories=categories, see_also=see_also, external_links=external_links, abstract=abstract)
