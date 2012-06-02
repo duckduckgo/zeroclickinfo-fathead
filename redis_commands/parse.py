d46bedca9387557da06156efa55b43cb21c95120
Dan Worth <djworth@gmail.com>
2012-06-02 06:34:37 -0400

    adding redis commands

---
+++ redis_commands/parse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import lxml.etree, lxml.html
+import re
+url = "http://redis.io"
+output = "output.txt"
+
+f = open(output, "w");
+
+tree = lxml.html.parse("download/raw.dat").getroot()
+commands = tree.find_class("command")
+
+data = {}
+
+for command in commands:
+
+    for row in command.findall('a'):
+        command_url = "%s/%s" % (url, row.get('href'))
+        for sibling in command.itersiblings():
+            data[command_url] = (row.text, sibling.text)
+
+for command_url in data.keys():
+    command, summary = data[command_url]
+    summary = unicode(summary).encode("utf-8");
+    f.write("\t".join([str(command),      # title
+                    "",                # namespace
+                    command_url,               # url
+                    summary,       # description
+                    "",                # synopsis
+                    "",                # details
+                    "",                # type
+                    ""                 # lang
+                   ])
+           );
+    f.write("\n");
+f.close()
