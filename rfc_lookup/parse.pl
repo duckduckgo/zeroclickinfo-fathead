56ffbf9802441471d350b4af73655a765c31acd3
Robert Picard <rpicard@live.com>
2012-06-05 16:24:14 -0400

    Merge pull request  #20 (rfc_lookup) with a few changes
    Categories were added, e.g. "RFCs from 1969"
    "Obsoleted" and "obsoleted by" information added
    Authors are listed
    Fix bug with "\0B" in RFC 6403 causing problems in the database

--- rfc_lookup/parse.pl
+++ rfc_lookup/parse.pl
@@ -14,21 +14,72 @@ foreach my $hr_rfc ( @rfcs ) {
 	my $id = $hr_rfc->{ 'doc-id' };
 	my $number = substr( $id, 3 ) + 0;
 	my $printable = "RFC $number";
+        my $categories = "RFCs from " . $hr_rfc->{'date'}->{'year'};
 	
+        my $dateString = $hr_rfc->{'date'}->{'month'} . " of " . $hr_rfc->{'date'}->{'year'};
+        my $titleString = "\"" . $hr_rfc->{'title'} . "\"";
+        
+        my @authors = ($hr_rfc->{'author'}->{'name'}) ? $hr_rfc->{'author'}->{'name'} : keys %{$hr_rfc->{'author'}};
+        my $authorString = "";
+       
+        # For debugging
+        
+        # print Dumper($hr_rfc->{'author'}, @authors);
+
+        my $i = 0;
+        
+        foreach my $author (@authors) {
+            if (!$i) {
+                $authorString = $author;
+            }
+            elsif ($i == @authors - 1) {
+                $authorString .= ", and " . $author;
+            }
+            else {
+                $authorString .= ", " . $author;
+            }
+            $i++;
+        }
+
+        my $obsoletes = substr($hr_rfc->{'obsoletes'}->{'doc-id'}, 3) + 0 if $hr_rfc->{'obsoletes'}->{'doc-id'};
+        my $obsoletedBy = substr($hr_rfc->{'obsoleted-by'}->{'doc-id'}, 3) + 0 if $hr_rfc->{'obsoleted-by'}->{'doc-id'};
+        
+        my $obsString = "";
+
+        if ($obsoletes && $obsoletedBy) {
+            $obsString = "It obsoletes <a href=\"/?q=RFC+$obsoletes\">RFC $obsoletes</a> and is obsoleted by <a href=\"/?q=RFC+$obsoletedBy\">RFC $obsoletedBy</a>. ";
+        }
+        elsif ($obsoletes) {
+            $obsString = "It obsoletes <a href=\"/?q=RFC+$obsoletes\">RFC $obsoletes</a>. ";
+        }
+        elsif ($obsoletedBy) {
+            $obsString = "It is obsoleted by <a href=\"/?q=RFC+$obsoletedBy\">RFC $obsoletedBy</a>. "
+        }
+
+
 	my $type = 'A';
-	
-	my $details = $hr_rfc->{'title'} . ", released "
-					 . $hr_rfc->{'date'}->{'month'} ." ". $hr_rfc->{'date'}->{'year'};
-	# TODO this parsing is fast/loose and doesn't handle missing/extra values
+
+	my $details = "$titleString was released in $dateString";
+
+        $details .= " by $authorString" if $authorString;
+
+        $details .= ". ";
+
+        $details .= $obsString;
+
+        
+        # TODO this parsing is fast/loose and doesn't handle missing/extra values
 	# needs to check each part more carefully!
 	
 	$details .= $hr_rfc->{'abstract'}->{'p'}->[0];
 	
-	# TODO, incorporate "obsoleted by" as further reading link
-	
+        # Hack to remove "\0B" from RFC 6403. It causes problems in the database
+        # and probably shouldn't be there in the first place
+        $details =~ s/\\0B//g if $number == 6403;
+
 	my $url = "http://www.rfc-editor.org/rfc/rfc$number.txt";
 	
-	print "$printable\t$type\t\t\t\t\t\t\t\t\t\t$details\t$url\n";
+	print "$number\t$type\t\t\t$categories\t\t\t\t\t\t\t$details\t$url\n";
 
 }
 # and that's all the news.
