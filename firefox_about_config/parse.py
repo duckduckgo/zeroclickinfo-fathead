1585464bc0edd1c78015d1ecd464bb5451d10a5b
Dylan Lloyd <dylan@dylansserver.com>
2012-06-25 23:05:05 -0400

    better parsing worse encoding

--- firefox_about_config/parse.py
+++ firefox_about_config/parse.py
@@ -2,6 +2,8 @@
 
 from BeautifulSoup import BeautifulSoup
 
+#print 'lol'.encode('utf-8')
+
 class Parser(object):
     def __init__(self, input='download/About:config_entries'):
         self.soup = BeautifulSoup(open(input))
@@ -18,14 +20,29 @@ class Parser(object):
                 i = 0
                 print
                 for th in tr.findAll('td'):
-                    if i == 0: print "name: ", th.b.contents
-                    elif i == 1: print "value: ", th.contents
+                    if i == 0:
+                        name = ''.join(th.b.findAll(text=True)).replace(' ','')
+                        print 'name: ', name
+                        file.write(name + '\t')
+                    elif i == 1:
+                        value = th.text
+                        print 'value: ', value
+                        file.write(value + '\t')
                     elif i == 2:
-                        print "description: ", th.contents
+                        description = ''
+                        for element in th.contents:
+                            try:
+                                description += " " + element
+                            except TypeError: 
+                                description += str(element)
+                        print "description: ", description
+                        file.write(description + '\n')
                         i = 0; continue
                     i += 1
 
 
 if __name__ == "__main__":
-    parser = Parser()
-    parser.findEntries()
+    with open('output.txt', 'w') as file:
+        parser = Parser()
+        parser.findEntries()
+
