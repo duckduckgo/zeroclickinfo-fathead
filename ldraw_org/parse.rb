fc8e8a5bb50429c60bbe451c6e7d327ce5b7db97
Robert Picard <mail@robert.io>
2012-06-26 21:23:42 -0400

    ldraw_org/parse.rb: More formatting changes

--- ldraw_org/parse.rb
+++ ldraw_org/parse.rb
@@ -72,14 +72,18 @@ rows.each do |row|
 	#   labels, and links to related/required files could be derived from the
 	#   detail page for each part, which would require more page scraping,
 	#   or also from actual files (best to report Tracker's interpretation)
-        abstract = "#{part_number}.dat is an unofficial LDraw #{part_type} titled \"#{part_name}.\" The current status for this #{part_type} is \"#{part_status}\""
+
+        # They are all "certified."
+        # part_status.gsub!(/.$/, '').downcase!
+
+        abstract = "#{part_number}.dat is an unofficial LDraw #{part_type} titled \"#{part_name}.\""
         
         if (part_name =~ /^(?:\~Moved to )(.*)$/) 
-            abstract = "#{part_number}.dat is an unofficial LDraw #{part_type} that has been moved to <a href=\"?q=ldraw+#{$1}\">#{$1}</a>."
+            abstract = "#{part_number}.dat is an unofficial LDraw #{part_type} that has been moved to <a href=\"?q=ldraw+#{$1}.dat\">#{$1}.dat</a>."
         end
 
         abstract.gsub!("\\", "")
-	
+
 	# If this is not the first part we've encountered with this name, append
 	# an abbreviated clarification/description to the existing part abstract.
 	# First instance of duplicates is arbitrarily given precedence.
@@ -90,8 +94,8 @@ rows.each do |row|
 		#   (or use part_name to look up number based on query by name?)
 		# - unclear what types are available/appropriate for line[1]
 		# - image and source links output relative to ldraw.org
-		parts[part_number] = [part_number, 'A', '', '', 'LDraw Parts', '', '', '', '', '', "[[Image: http://ldraw.org/library/unofficial/images/#{part_path}/#{part_number}.png]]", abstract, part_link];
 		parts["#{part_number}.dat"] = ["#{part_number}.dat", 'A', '', '', 'LDraw Parts', '', '', '', '', '', "[[Image: http://ldraw.org/library/unofficial/images/#{part_path}/#{part_number}.png]]", abstract, part_link];
+		parts[part_number] = [part_number, 'R', "#{part_number}.dat", '', '', '', '', '', '', '', "",'' ,'' ];
 #	end
 	
 end
