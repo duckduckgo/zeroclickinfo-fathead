f827fbac124ac3de8b7edb6d35137c0ef3dea064
mr.Shu <mrshux@gmail.com>
2012-07-12 09:16:25 +0200

    fetch.sh cleaned

--- aur/fetch.sh
+++ aur/fetch.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-echo $(wget -qO- http://aur.archlinux.org/packages.php| grep 'a class=.*Next'|sed 's/.*href=\(.*\).*/\1/g'|sed 's#"\(.*\)">Next</a>#\1#g')
-#       LAST=$(wget -qO- http://aur.archlinux.org/packages.php| grep 'a class=.*Last'| sed 's/^.*;O=\([0-9]*\).*$/\1/g')
-#       echo $LAST
-
-
-
 
 wget -q -P download -N 'http://aur.archlinux.org/packages.php'
 URL='packages.php';
 URL=$(cat "download/$URL" | grep 'a class=.*Next'|sed 's/.*href=\(.*\).*/\1/g'|sed 's#"\(.*\)">Next</a>#\1#g'|sed 's#&amp;#&#g'|sed 's#%3B#;#g'|sed 's#amp;##g')
-echo $URL
+#echo $URL
 
 while test -n "$URL"; 
 do
         wget -q -P download -N "http://aur.archlinux.org/$URL"
         URL=$(cat "download/$URL" | grep 'a class=.*Next'|sed 's/.*href=\(.*\).*/\1/g'|sed 's#"\(.*\)">Next</a>#\1#g'|sed 's#&amp;#&#g'|sed 's#%3B#;#g'|sed 's#amp;##g')
-        echo $URL;
+        #echo $URL;
 done
