d6075c27b403ff4f44aed9cf8a5f3752356d0663
Robert Picard <mail@robert.io>
2012-07-25 16:22:55 -0400

    Using a "passed" branch instead of a folder

--- passed/alexa_rank/README.txt
+++
@@ -1,7 +0,0 @@
-Dependencies:
-
-Python 3
-
-To install in Ubuntu 10.04 (Lucid):
-
-sudo apt-get install python3-minimal
--- passed/iusethis_com/fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# rm -rf download
-mkdir -p download
-# Note - just picked 30 pages arbitrarily for the moment
-curl "http://{osx,iphone,windows}.iusethis.com/?page=[1-30]" --output "download/iusethis_#1_#2.html"
-
