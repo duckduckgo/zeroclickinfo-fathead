5e0f2f777fb0e2bace42c097530d605ecc0b1d91
Dylan Lloyd <dylan@dylansserver.com>
2012-07-31 21:55:36 -0400

    working on output

--- pci_ids/parse.pl
+++ pci_ids/parse.pl
@@ -5,22 +5,33 @@ use strict;
 
 open my $fh, '<', 'download/pci.ids';
 
-my (%vendors, %devices, %subdevices) = ();
+my (%vendors) = ();
+my (@devices, @subdevices) = [];
 my %key = (
     0 => sub { s/^(....)\ //; $vendors{$1} = $_; },
-    1 => sub { s/^(....)\ //; $devices{$1} = $_; },
+    1 => sub { s/^(....)\ //; push @devices, { $1 => $_ } },
     2 => sub {
         s/^(....)\ (....)\ //;
         return if not $2;
-        $subdevices{"2"} = {
+        push @subdevices, {
             "subvendor" => $1,
             "subsystem_name" => $_ };
     },
 );
-while (<$fh>) { 
-    next if /^#|^\s*$/;
-    $key{length $1}->(s/^(\t{0,2})//);
-    foreach my $vendor (keys %vendors) {
-        print "$vendor -> $vendors{$vendor}";
-    }
+while (<$fh>) { $key{length $1}->(s/^(\t{0,2})//); }
+foreach my $vendor (keys %vendors) {
+    print $vendor . " ";
+    print keys shift @devices;
+    print " -> $vendors{$vendor}";
+##        print "\n$vendor ";
+##        if (scalar @devices == 1) {
+##            print keys @devices;
+#        print "device";
+#        print scalar @devices;
+#        if (scalar @devices == 2) {
+#            my $device = shift @devices;
+#            print $device;
+#        } else { print "\n"; }
+##        }
+#    #next if scalar @devices == 0;
 }
