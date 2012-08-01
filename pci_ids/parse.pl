c711e4228d38b5e14767209eb0b6419cff6c9cbb
Dylan Lloyd <dylan@dylansserver.com>
2012-08-01 16:20:51 -0400

    push all info into arrays, much better

--- pci_ids/parse.pl
+++ pci_ids/parse.pl
@@ -5,11 +5,10 @@ use strict;
 
 open my $fh, '<', 'download/pci.ids';
 
-my (%vendors) = ();
-my (@devices, @subdevices) = [];
+my (@vendors, @devices, @subdevices) = [];
 my %key = (
-    0 => sub { s/^(....)\ //; $vendors{$1} = $_; },
-    1 => sub { s/^(....)\ //; push @devices, { $1 => $_ } },
+    0 => sub { s/^(....)\ //; push @vendors, { "id" => $1, "name" => $_ } },
+    1 => sub { s/^(....)\ //; push @devices, { "id" => $1, "name" => $_ } },
     2 => sub {
         s/^(....)\ (....)\ //;
         return if not $2;
@@ -18,20 +17,13 @@ my %key = (
             "subsystem_name" => $_ };
     },
 );
-while (<$fh>) { $key{length $1}->(s/^(\t{0,2})//); }
-foreach my $vendor (keys %vendors) {
-    print $vendor . " ";
-    print keys shift @devices;
-    print " -> $vendors{$vendor}";
-##        print "\n$vendor ";
-##        if (scalar @devices == 1) {
-##            print keys @devices;
-#        print "device";
-#        print scalar @devices;
-#        if (scalar @devices == 2) {
-#            my $device = shift @devices;
-#            print $device;
-#        } else { print "\n"; }
-##        }
-#    #next if scalar @devices == 0;
+while (<$fh>) {
+    next if m/^#|^\s+$/;
+    $key{length $1}->(s/^(\t{0,2})//)
+}
+my $line = 0;
+foreach my $vendor (@vendors) {
+    next if ++$line == 1;
+    print %{$vendor}->{id} . " ";
+    print %{$vendor}->{name} . "\n";
 }
