82b7007c3c6fd3bbd68ee5e7285499ce857c184a
Dylan Lloyd <dylan@dylansserver.com>
2012-08-02 22:21:28 -0400

    fixed all the pesky problems

--- pci_ids/parse.pl
+++ pci_ids/parse.pl
@@ -8,21 +8,21 @@ open my $fh, '<', 'download/pci.ids';
 my (@vendors, @devices, @subdevices) = ();
 my %key = (
     0 => sub {
-        s/^(....)\ //;
+        s/^(....)\ //; chomp;
         push @vendors, {
             "id" => $1,
             "name" => $_,
             "devices" => [] }
     },
     1 => sub {
-        s/^(....)\ //;
+        s/^(....)\ //; chomp;
         push $vendors[-1]{devices}, {
             "id" => $1,
             "name" => $_,
             "subdevices" => [] }
     },
     2 => sub {
-        s/^(....)\ (....)\ //;
+        s/^(....)\ (....)\ //; chomp;
         return if not $2;
         push $vendors[-1]{devices}[-1]{subdevices}, {
             "subvendor" => $1,
@@ -39,18 +39,18 @@ foreach my $vendor (@vendors) {
         foreach my $device (@{$vendor->{devices}}) {
             if (scalar @{$device->{subdevices}} > 0) {
                 foreach my $subdevice (@{$device->{subdevices}}) {
-                    print $vendor->{id} . " $device->{id} "
-                        . $subdevice->{subvendor} . " "
-                        . $subdevice->{subdevice} . " "
-                        . $vendor->{name} . " $device->{name} "
-                        . $subdevice->{subsystem_name};
+                    print "$vendor->{id} $device->{id} "
+                        . "$subdevice->{subvendor} "
+                        . "$subdevice->{subdevice} "
+                        . "$vendor->{name} $device->{name} "
+                        . "$subdevice->{subsystem_name}\n";
                 }
             } else {
-                print $vendor->{id} . " $device->{id}"
-                    . (chomp $device->{name}) . " " . $vendor->{name};
+                print "$vendor->{id} $device->{id}"
+                    . "$device->{name} $vendor->{name}\n";
             }
         }
     } else {
-        print $vendor->{id} . " " . $vendor->{name};
+        print "$vendor->{id} $vendor->{name}\n";
     }
 }
