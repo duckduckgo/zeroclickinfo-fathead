78886cb9299edfd8e77491b723303e6f02d6d757
Dylan Lloyd <dylan@dylansserver.com>
2012-08-02 23:13:33 -0400

    added title => abstract hash

--- pci_ids/parse.pl
+++ pci_ids/parse.pl
@@ -6,23 +6,24 @@ use strict;
 open my $fh, '<', 'download/pci.ids';
 
 my (@vendors, @devices, @subdevices) = ();
+my %queries = ();
 my %key = (
     0 => sub {
-        s/^(....)\ //; chomp;
+        s/^(....)\s+//; chomp;
         push @vendors, {
             "id" => $1,
             "name" => $_,
             "devices" => [] }
     },
     1 => sub {
-        s/^(....)\ //; chomp;
+        s/^(....)\s+//; chomp;
         push $vendors[-1]{devices}, {
             "id" => $1,
             "name" => $_,
             "subdevices" => [] }
     },
     2 => sub {
-        s/^(....)\ (....)\ //; chomp;
+        s/^(....)\ (....)\s+//; chomp;
         return if not $2;
         push $vendors[-1]{devices}[-1]{subdevices}, {
             "subvendor" => $1,
@@ -36,10 +37,13 @@ while (<$fh>) {
 }
 foreach my $vendor (@vendors) {
     print "$vendor->{id} $vendor->{name}\n";
+    $queries{$vendor->{id}} = $vendor->{name};
     if (scalar @{$vendor->{devices}} > 0) {
         foreach my $device (@{$vendor->{devices}}) {
             print "$vendor->{id} $device->{id}"
                 . "$device->{name} $vendor->{name}\n";
+            $queries{"$vendor->{id} $device->{id}"} =
+                "$device->{name} $vendor->{name}";
             if (scalar @{$device->{subdevices}} > 0) {
                 foreach my $subdevice (@{$device->{subdevices}}) {
                     print "$vendor->{id} $device->{id} "
@@ -47,6 +51,11 @@ foreach my $vendor (@vendors) {
                         . "$subdevice->{subdevice} "
                         . "$vendor->{name} $device->{name} "
                         . "$subdevice->{subsystem_name}\n";
+                    $queries{"$vendor->{id} $device->{id}"
+                            . "$subdevice->{subvendor} "
+                            . "$subdevice->{subdevice} "} =
+                                "$device->{name} $vendor->{name}"
+                                . "$subdevice->{subsystem_name}";
                 }
             }
         }
