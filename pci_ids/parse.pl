d2e35d8d60469f0c8a8cdeb4456d251723c4d671
Dylan Lloyd <dylan@dylansserver.com>
2012-08-05 03:40:41 -0400

    added categories, empties are still included

--- pci_ids/parse.pl
+++ pci_ids/parse.pl
@@ -3,6 +3,8 @@
 use warnings;
 use strict;
 
+use Data::Dumper;
+
 open my $fh, '<', 'download/pci.ids';
 
 my @vendors = ();
@@ -35,16 +37,23 @@ while (<$fh>) {
     next if m/^#|^\s+$/;
     $key{length $1}->(s/^(\t{0,2})//);
 }
+my $i = 0;
 foreach my $vendor (@vendors) {
+    print ++$i . "\n";
 #    print "$vendor->{id} $vendor->{name}\n";
-    $queries{$vendor->{id}} = "Vendor: $vendor->{name}";
+    $queries{$vendor->{id}} = {
+        "abstract" =>"Vendor: $vendor->{name}",
+        "category" => "PCI vendor ID $vendor->{id}"
+    };
     if (scalar @{$vendor->{devices}} > 0) {
         foreach my $device (@{$vendor->{devices}}) {
 #            print "$vendor->{id} $device->{id}"
 #                . "$device->{name} $vendor->{name}\n";
-            $queries{"$vendor->{id} $device->{id}"} =
-                "Vendor: $vendor->{name}<br>"
-                . "Device: $device->{name}";
+            $queries{"$vendor->{id} $device->{id}"} = {
+                "abstract" => "Vendor: $vendor->{name}<br>"
+                            . "Device: $device->{name}",
+                "category" => "PCI vendor ID $vendor->{id}"
+            };
             if (scalar @{$device->{subdevices}} > 0) {
                 foreach my $subdevice (@{$device->{subdevices}}) {
 #                    print "$vendor->{id} $device->{id} "
@@ -54,11 +63,14 @@ foreach my $vendor (@vendors) {
 #                        . "$subdevice->{subsystem_name}\n";
                     $queries{"$vendor->{id} $device->{id}"
                             . "$subdevice->{subvendor} "
-                            . "$subdevice->{subdevice} "} =
-                                "Vendor: $vendor->{name}<br>"
-                                . "Device: $device->{name}<br>"
-                                . "Subdevice/subvendor: "
-                                . "$subdevice->{subsystem_name}";
+                            . "$subdevice->{subdevice}"} = {
+                                "abstract" => "Vendor: $vendor->{name}<br>"
+                                    . "Device: $device->{name}<br>"
+                                    . "Subdevice/subvendor: "
+                                    . "$subdevice->{subsystem_name}",
+                                "category" => "PCI vendor ID "
+                                             . "$vendor->{id}"
+                    };
                 }
             }
         }
@@ -68,19 +80,19 @@ foreach my $vendor (@vendors) {
 open my $output, '>', 'output.txt';
 map {
     print $output join "\t", (
-        "$_",                               # title
+        $_,                                 # title
         "A",                                # type
         "",                                 # redirect
         "",                                 # otheruses
-        "",                                 # categories
+        $queries{$_}->{category},           # categories
         "",                                 # references
         "",                                 # see_also
         "",                                 # further_reading
         "",                                 # external_links
         "",                                 # disambiguation
         "",                                 # images
-        "$queries{$_}",                     # abstract
-        "http://pciids.sourceforge.net/\n"    # source_url
+        $queries{$_}->{abstract},           # abstract
+        "http://pciids.sourceforge.net/\n"  # source_url
     );
 } keys %queries;
 
