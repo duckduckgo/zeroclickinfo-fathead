e19ab9136c3a1b7e765408fd2d85fc87a25f6ee5
Jessica Yu <kumimoko.yo@gmail.com>
2012-08-13 13:54:17 -0700

    added use strict; use warnings

--- unix_man/parse.pl
+++ unix_man/parse.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/env perl 
-use encoding "utf8";
+use encoding 'utf8';
+use strict;
+use warnings;
 use Cwd qw(chdir);
 # for each man page file, grab Name, and Synopsis if possible
-chdir downloads;
-@cmdlist = `ls`; 
-
+chdir 'downloads';
+my @cmdlist = `ls`; 
+my $cmd;
 sub parse_name {
-	for ($str = $_[0]) { 
+	my $str = $_[0];
+	for ($str) { 
 		s/<[a-zA-Z]*>//g;
 		s/<\/[a-zA-Z]*>//g;	
 		s/[\s]+[-]+[\s]+/\t/g;	
@@ -15,7 +18,8 @@ sub parse_name {
 	return $str;
 }
 sub parse_syn {
-	for ($str = $_[0]) {
+	my $str = $_[0];
+	for ($str) {
 		s/[\n\t]//g;
 		s/<[a-zA-Z\s"'!-]*>//g;
 		s/<[0-9a-zA-Z:_"=\/-;\s!.]*>//g;
@@ -27,16 +31,21 @@ sub parse_syn {
 	}
 	return $str;
 }
+
 # for each HTML manpage in downloads/
 foreach $cmd (@cmdlist) 
 {
-	open MANPAGE, "$cmd" or die "Cannot open file: $cmd";
+	open MANPAGE, $cmd or die "Cannot open file: $cmd";
 	# print the Name section of each manpage. 
 	# if there's a Synopsis, print that too. stop at the next <h2>, or bail if the synopsis is too long. (max 5 lines) 
 	# I imposed the 5 line limit for the Synopsis because some commands have a REALLY long one, 
 	# and it wouldn't be suitable for a fathead plugin.
 
 	# For each line in the manpage,
+	my ($line, $nextline, $name, $max, @synopsis);
+	$line = $nextline = $name = '';
+	$max = 0;
+	@synopsis = ();
 	while ($line = <MANPAGE>)
 	{
 		# If you find the name section...
@@ -52,13 +61,11 @@ foreach $cmd (@cmdlist)
 
 		# Continuing with the same manpage, If you find the Synopsis section....
 		if ($line =~ m/<h2>Synopsis/) {
-			$max = 0;
 			if ($nextline =~ m/^[\s\t]*$/ || $nextline =~ m/^$/) {
 				while ($nextline =~ m/^[\s\t]*$/ || $nextline =~ m/^$/) {
 					$nextline = <MANPAGE>;
 				}
 			}
-			@synopsis = ();
 				while (!($nextline =~ m/<h[2-3]>/)) {
 					last if ($max > 5);
 					$nextline = &parse_syn($nextline);
