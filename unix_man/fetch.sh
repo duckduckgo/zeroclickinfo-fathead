b84a77f3b64c421800208858c453c9b09b617e12
Jessica Yu <kumimoko.yo@gmail.com>
2012-08-21 01:05:36 -0700

    switched data source to linuxcommand.org
    Signed-off-by: Robert Picard <mail@robert.io>

--- unix_man/fetch.sh
+++ unix_man/fetch.sh
@@ -1,18 +1,16 @@
-mkdir -p download
-cd download
-#
-# Other possible sources....
-# linuxcommand.org,
+#!/bin/bash 
 
-# Chose linux.die.net as source, as it was the only site I found 
-# that had static html man pages and I could crawl it for the most part (with restrictions). 
 
-# --user-agent="" is an option, but die.net refuses
-# I couldn't crawl the entire man page database, 
-# as it is HUGE and I was blocked twice 
-# for most likely sending way too many GET requests within a certain time period. 
+# fetch from first source, linuxcommand.org
+wget -rqP download --reject php,images,jpg,js,css,png,ico,gif,style --wait 1 -nH -nd http://linuxcommand.org/smp_{a..z}.php
 
-# However their robots.txt they allow DuckDuckGo to crawl the site so the -e robots=off option could probably be taken off.
-# consider --header="accept-encoding: gzip" for compression, I couldn't test it because 
-# linux.die.net only allows certain agents to crawl their site
-wget -e robots=off --reject html,css,png,ico,gif,style --wait 3 -nH -r http://linux.die.net/man/ 
+
+# just in case a second source is needed in the future, tldp.org
+#wget --wait 1 -nH -nd -r http://www.tldp.org/manpages/man-html/man-html-20070604.tar.bz2 
+# extract and prepare pages for parsing 
+#tar -xvjf man-html-20070604.tar.bz2 
+#mv man-html-20070604 tldp
+#mv tldp/html/htmlman{1..5}/* tldp
+#mv tldp/html/htmlman{7..9}/* tldp
+#rm -rf tldp/html
+
