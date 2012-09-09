065f77b08ca56a3e211ccc9974a9dc2078ae525d
Jessica Yu <kumimoko.yo@gmail.com>
2012-09-09 02:40:02 -0700

    separate parsing for bash built-ins

--- unix_man/parse.pl
+++ unix_man/parse.pl
@@ -14,6 +14,11 @@ sub strip {
 	return $_[0];
 }
 
+my @builtins = ( 'alias', 'bg', 'bind','break', 'builtin', 'cd', 'command', 'compgen', 'complete', 'continue', 'declare','dirs', 'disown', 'enable', 'eval', 'exec', 'exit', 'export', 'fc', 'getopts', 'hash', 'help', 'history', 'jobs', 'let', 'local', 'local', 'logout', 'popd', 'pushd', 'read', 'readonly', 'return', 'set', 'shift', 'shopt', 'source', 'suspend', 'times', 'trap', 'type', 'typeset', 'ulimit', 'umask', 'unalias', 'unset', 'wait', 'fg' ); 
+my %builtins;
+foreach (@builtins) {
+	$builtins{$_} = 1;
+}
 my @cmdlist = `ls download`;
 chomp(@cmdlist);
 # for each HTML manpage in download/
@@ -28,11 +33,32 @@ foreach my $page (@cmdlist)
 	my $section = $page;
 	$page =~ s/[0-9][.]html//;
 	$section =~ s/[a-z0-9A-Z]*([0-9])[.]html/$1/;
+	if (exists $builtins{$page})	{
+		while ($line = <MANPAGE>) {
+			if ($line =~ m/^[\s]*<B>$page/ && ($line =~ m/.*\[+/ || $line =~ m/<I>/) ) {
+				$line =~ s/^[\s\t]+//;
+				chomp($line);
+				$line =~ s/$/<br \/>/;
+				push(@synopsis, $line);
+				$line = <MANPAGE>;
+				while ($line =~ m/<B>$page/ || $line =~ m/\[+/) {
+					chomp($line); 
+					$line =~ s/^[\s\t]+//;
+					$line =~ s/$/<br \/>/;
+					push(@synopsis, $line);
+					$line = <MANPAGE>;
+				}
+				last;
+
+			}
+		}
+		
+	} else {
 	# For each line in the manpage,
 	while ($line = <MANPAGE>)
 	{
 		# If you find the name section...
-		if ($line =~ m/<h2>Name/i) {
+		if ($line =~ m/<h2>Name/i || $line =~ m/NAME/) {
 			$nextline = <MANPAGE>;
 			while ($nextline =~ /^[\s\t]*$/ || $nextline =~ /^$/) { # skip the blank lines.
 				$nextline = <MANPAGE>;
@@ -40,32 +66,38 @@ foreach my $page (@cmdlist)
 			$description = &strip($nextline);
 		}
 		# Continuing with the same manpage, If you find the Synopsis section....
-		if ($line =~ m/<h2>Synopsis/i) {
+		if ($line =~ m/<h2>Synopsis/i || $line =~ m/SYNOPSIS/) {
 			$nextline = <MANPAGE>;
+			while ($nextline =~ /^[\s\t]*$/ || $nextline =~ /^$/) { # skip the blank lines.
+				$nextline = <MANPAGE>;
+			}
 			while (!($nextline =~ m/<h2>/i) && !($nextline =~ m/^[\s\t]+$/) && !($nextline =~ m/^$/)) {
 				last if ($max > 8);
 				$nextline = &strip($nextline);
 				if ($nextline =~ /^[\s\t]*$/ || $nextline =~ /^$/) {
 					next;
 				}
+				$nextline =~ s/[\s][\s]*/ /g;
 				$nextline =~ s/$/<br \/>/;
 				push(@synopsis, $nextline);
 				$max++;
 			} continue {
 				$nextline = <MANPAGE>;
 			}
-			$synopsis[-1] =~ s/<br \/>//g if @synopsis >= 1;
 			last;
 		}
 	}
+}
+	next if (!$description && !@synopsis);	
+	$synopsis[-1] =~ s/<br \/>//g if @synopsis >= 1;
 	my $url="http://linuxcommand.org/man_pages/$page".$section.'.html';
 	# If output is borked somehow and you need it in unicode, uncomment the next line.
 	# binmode(STDOUT, ":utf8");
 	print "$page\tA\t\t\t\t\t\t\t\t\t\t";
 	print "<pre><code>@synopsis</code></pre>" if (@synopsis);
 	print "$page($section) is a Bash built-in.<br />" if ($description =~ m/^bash,/); 
-	print "$description\t$url\n";
-	# print "$page\t\t$url\t$description\t@synopsis\t\t\t\n";
+	print "$description\t" if ($description);
+	print "$url\n";
 	close (MANPAGE);
 }
 exit 0;
