ca7f04d24694cf866bb019839c8469a6cdfa3f0c
Jessica Yu <kumimoko.yo@gmail.com>
2012-09-27 11:11:25 -0700

    fixed code w/ suggestions

--- unix_man/parse.pl
+++ unix_man/parse.pl
@@ -6,16 +6,12 @@ use Encode;
 # for each man page file, grab Name, and Synopsis if possible
 
 sub strip {
-	for ($_[0]) { 
-		s/<[a-zA-Z]*>//g;
-		s/<\/[a-zA-Z]*>//g;	
-		s/[\s\t]*$//g;
-		s/^[\s\t]*//g;
-	}
-	return $_[0];
+	my $line = shift;
+	$line =~ s/<[a-zA-Z]*>|<\/[a-zA-Z]*>|[\s\t]*$|^[\s\t]*//g;
+	return $line; 
 }
 
-my @builtins = ( 'alias', 'bg', 'bind','break', 'builtin', 'cd', 'command', 'compgen', 'complete', 'continue', 'declare','dirs', 'disown', 'enable', 'eval', 'exec', 'exit', 'export', 'fc', 'getopts', 'hash', 'help', 'history', 'jobs', 'let', 'local', 'local', 'logout', 'popd', 'pushd', 'read', 'readonly', 'return', 'set', 'shift', 'shopt', 'source', 'suspend', 'times', 'trap', 'type', 'typeset', 'ulimit', 'umask', 'unalias', 'unset', 'wait', 'fg' ); 
+my @builtins = qw( alias bg bindbreak builtin cd command compgen complete continue declaredirs disown enable eval exec exit export fc getopts hash help history jobs let local local logout popd pushd read readonly return set shift shopt source suspend times trap type typeset ulimit umask unalias unset wait fg ); 
 my %builtins;
 foreach (@builtins) {
 	$builtins{$_} = 1;
@@ -25,7 +21,7 @@ chomp(@cmdlist);
 # for each HTML manpage in download/
 foreach my $page (@cmdlist) 
 {
-	open MANPAGE, "download/$page" or die "Cannot open file: $page";
+	open my $manpage, "download/$page" or die "Cannot open file: $page";
 	# print the Name section of each manpage. 
 	# if there's a Synopsis, print that too. stop at the next <h2>, or bail if the synopsis is too long. (max 5 lines) 
 	my $max = 0;
@@ -35,19 +31,19 @@ foreach my $page (@cmdlist)
 	$page =~ s/[0-9][.]html//;
 	$section =~ s/[a-z0-9A-Z]*([0-9])[.]html/$1/;
 	if (exists $builtins{$page})	{
-		while ($line = encode('utf8', <MANPAGE>)) {
+		while ($line = encode('utf8', <$manpage>)) {
 			if ($line =~ m/^[\s]*<B>$page/ && ($line =~ m/.*\[+/ || $line =~ m/<I>/) ) {
 				$line =~ s/^[\s\t]+//;
 				chomp($line);
 				$line =~ s/$/<br \/>/;
 				push(@synopsis, $line);
-				$line = <MANPAGE>;
+				$line = <$manpage>;
 				while ($line =~ m/<B>$page/ || $line =~ m/\[+/) {
 					chomp($line); 
 					$line =~ s/^[\s\t]+//;
 					$line =~ s/$/<br \/>/;
 					push(@synopsis, $line);
-					$line = <MANPAGE>;
+					$line = <$manpage>;
 				}
 				last;
 
@@ -56,28 +52,28 @@ foreach my $page (@cmdlist)
 		
 	} else {
 	# For each line in the manpage,
-	while ($line = <MANPAGE>)
+	while ($line = <$manpage>)
 	{
 		# If you find the name section...
 		if ($line =~ m/<h2>Name/i || $line =~ m/NAME/) {
-			$nextline = <MANPAGE>;
+			$nextline = <$manpage>;
 			while ($nextline =~ /^[\s\t]*$/ || $nextline =~ /^$/) { # skip the blank lines.
-				$nextline = <MANPAGE>;
+				$nextline = <$manpage>;
 			}
-				while (!($nextline =~ m/<h2>/i) && !($nextline =~ m/^[\s\t]+$/) && !($nextline =~ m/^$/)) {
+				while ($nextline !~ m/<h2>/i && $nextline !~ m/^[\s\t]+$/ && $nextline !~ m/^$/) {
 				$description .= &strip($nextline);
-				last if !($nextline = <MANPAGE>);
+				last if !($nextline = <$manpage>);
 				}
 		}
 		# Continuing with the same manpage, If you find the Synopsis section....
 		if ($line =~ m/<h2>Synopsis/i || $line =~ m/SYNOPSIS/) {
-			$nextline = <MANPAGE>;
+			$nextline = <$manpage>;
 			while ($nextline =~ /^[\s\t]*$/ || $nextline =~ /^$/) { # skip the blank lines.
-				$nextline = <MANPAGE>;
+				$nextline = <$manpage>;
 			}
-			while (!($nextline =~ m/<h2>/i) && !($nextline =~ m/^[\s\t]+$/) && !($nextline =~ m/^$/)) {
+			while ($nextline !~ m/<h2>/i && $nextline !~ m/^[\s\t]+$/ && $nextline !~ m/^$/) {
 				last if ($max > 8);
-				$nextline = &strip($nextline);
+				$nextline = strip($nextline);
 				if ($nextline =~ /^[\s\t]*$/ || $nextline =~ /^$/) {
 					next;
 				}
@@ -86,7 +82,7 @@ foreach my $page (@cmdlist)
 				push(@synopsis, $nextline);
 				$max++;
 			} continue {
-				$nextline = <MANPAGE>;
+				$nextline = <$manpage>;
 			}
 			last;
 		}
@@ -102,6 +98,5 @@ foreach my $page (@cmdlist)
 	# print "$page\t\t$url\t$description\t@synopsis\t\t\t\n";
 	print "$description\t" if ($description);
 	print "$url\n";
-	close (MANPAGE);
+	close ($manpage);
 }
-exit 0;
