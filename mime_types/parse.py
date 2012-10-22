356da8091eb199bff4d795bb5cdd94d74ab1c4b2
vaishak <vaishak@vaishak-ThinkPad-L420.(none)>
2012-10-22 13:12:00 +0530

    Removed 'MIME type' from title
    Signed-off-by: Robert Picard <mail@robert.io>

--- mime_types/parse.py
+++ mime_types/parse.py
@@ -4,5 +4,5 @@ mimetypes.init()
 OUTPUT = "output.txt"
 fout = open(OUTPUT,"w")
 for extention, mimetype in mimetypes.types_map.iteritems():
-	fout.write("\t".join(["MIME type "+extention, "A", "", "", "", "", "", "", "", "", "", "", 
+	fout.write("\t".join([extention, "A", "", "", "", "", "", "", "", "", "", "", 
 	"The MIME type for this "+extention+" is "+mimetype, ""])+"\n")
