7f54d32e86a45bec96751770df22543334337606
Robert Picard <mail@robert.io>
2012-10-26 16:57:53 -0400

    Add a period to the end of the sentence.

--- mime_types/parse.py
+++ mime_types/parse.py
@@ -4,4 +4,4 @@ mimetypes.init()
 OUTPUT = "output.txt"
 fout = open(OUTPUT,"w")
 for extension, mimetype in mimetypes.types_map.iteritems():
-	fout.write("\t".join([extension, "A", "", "", "", "", "", "", "", "", "", "The MIME type for the extension <code>"+extension+"</code> is <code>"+mimetype+"</code>", "http://hg.python.org/cpython/file/2.7/Lib/mimetypes.py"])+"\n")
+	fout.write("\t".join([extension, "A", "", "", "", "", "", "", "", "", "", "The MIME type for the extension <code>"+extension+"</code> is <code>"+mimetype+"</code>.", "http://hg.python.org/cpython/file/2.7/Lib/mimetypes.py"])+"\n")
