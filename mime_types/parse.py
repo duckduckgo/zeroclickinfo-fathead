4667a3dd7bff910289b2e074d8745f24f5c850e1
vaishak <vaishak@vaishak-ThinkPad-L420.(none)>
2012-10-27 12:35:42 +0530

    merged

--- mime_types/parse.py
+++ mime_types/parse.py
@@ -4,9 +4,6 @@ mimetypes.init()
 OUTPUT = "output.txt"
 fout = open(OUTPUT,"w")
 for extension, mimetype in mimetypes.types_map.iteritems():
-<<<<<<< HEAD
 	fout.write("\t".join([extension, "A", "", "", "", "", "", "", "", "", "", "", 
 	"The MIME type for the extension <code>"+extension+"</code> is <code>"+mimetype+"</code>", ""])+"\n")
-=======
 	fout.write("\t".join([extension, "A", "", "", "", "", "", "", "", "", "", "The MIME type for the extension <code>"+extension+"</code> is <code>"+mimetype+"</code>.", "http://hg.python.org/cpython/file/2.7/Lib/mimetypes.py"])+"\n")
->>>>>>> 7f54d32e86a45bec96751770df22543334337606
