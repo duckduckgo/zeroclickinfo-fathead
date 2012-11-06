78d6442b83c261a046faa34f2a14a69cdc1a5eb8
Robert Picard <mail@robert.io>
2012-11-06 13:38:39 -0500

    Add meta information for FileFormats

---
+++ file_formats/FileFormats.pm
@@ -0,0 +1,28 @@
+package DDG::Fathead::FileFormats;
+
+use DDG::Fathead;
+
+primary_example_queries => "file .cat";
+
+secondary_example_queries =>
+    "file extension .pdf",
+    "psd file";
+
+description "File extension descriptions";
+
+name "FileFormats";
+
+icon_url "/assets/icon_wikipedia.v101.png";
+
+source "Wikipedia";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/file_formats";
+
+topics => "everyday_goodies", "geek", "sysadmin";
+
+categories => "computing_info", "software";
+
+attribution
+    github => ['https://github.com/whee', 'whee'];
+
+1;
