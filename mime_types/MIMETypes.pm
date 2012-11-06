e671f46af94d17757315ae4a9882652a3d6010d2
Robert Picard <mail@robert.io>
2012-11-06 15:06:58 -0500

    Add meta information for MIMETypes

---
+++ mime_types/MIMETypes.pm
@@ -0,0 +1,29 @@
+package DDG::Fathead::MIMETypes;
+
+use DDG::Fathead;
+
+primary_example_queries => ".jpg mime";
+
+secondary_example_queries =>
+    "mime type pdf",
+    "mp4 mime";
+
+description "MIME type reference";
+
+name "MIMETypes";
+
+icon_url "/assets/icon_wikipedia.v101.png";
+
+source "Wikipedia";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/mime_types";
+
+topics => "geek", "web_design", "special_interest";
+
+category => "reference";
+
+attribution
+    github => ['https://github.com/vaishaks', 'vaishaks'],
+    twitter => ['https://twitter.com/vaishaks', 'vaishaks'];
+
+1;
