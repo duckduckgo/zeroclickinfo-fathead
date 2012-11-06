9c8f66a3f87662159e914aa268475bf9ddf8ad64
Robert Picard <mail@robert.io>
2012-11-06 13:28:28 -0500

    Add meta information for Bible

---
+++ bible/Bible.pm
@@ -0,0 +1,31 @@
+package DDG::Fathead::Bible;
+
+use DDG::Fathead;
+
+primary_example_queries => "genesis 1:11";
+
+secondary_example_queries =>
+    "gen 1:12",
+    "gen 1 12",
+    "bible genesis 11:2",
+    "gen 2:13 bible";
+
+description "Bible verses";
+
+name "Bible";
+
+icon_url "/i/blueletterbible.org.ico";
+
+source "Blue Letter Bible";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/bible";
+
+topics => "everyday_goodies", "special_interest";
+
+categories => "reference";
+
+attribution
+    github => ['https://github.com/MithrandirAgain', 'MithrandirAgain'],
+    twitter => ['https://twitter.com/mithrandiragain', 'mithrandiragain'];
+
+1;
