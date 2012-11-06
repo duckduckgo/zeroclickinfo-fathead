f679bd64c6a5f191a5e9f6fba54592b66316ea54
Robert Picard <mail@robert.io>
2012-11-06 14:05:15 -0500

    Add meta information for HelloWorld

---
+++ hello_world/HelloWorld.pm
@@ -0,0 +1,29 @@
+package DDG::Fathead::HelloWorld;
+
+use DDG::Fathead;
+
+primary_example_queries => "hello world perl";
+
+secondary_example_queries =>
+    "javascript hello world",
+    "hello world in c";
+
+description "Hello World programs in many program languages";
+
+name "HelloWorld";
+
+icon_url "/i/www.github.com.ico";
+
+source "GitHub";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/hello_world";
+
+topics => "geek", "programming";
+
+categories => "computing_info", "programming", "software";
+
+attribution
+    twitter => ['https://twitter.com/jperla', 'jperla'],
+    web => ['http://www.jperla.com/blog', 'Joseph Perla'];
+
+1;
