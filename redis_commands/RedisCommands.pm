5e03cc446637675d2deb2431b882c2adfb01d4b0
Robert Picard <mail@robert.io>
2012-11-06 15:50:39 -0500

    Add meta information for RedisCommands

---
+++ redis_commands/RedisCommands.pm
@@ -0,0 +1,29 @@
+package DDG::Fathead::RedisCommands;
+
+use DDG::Fathead;
+
+primary_example_queries => "redis rpush";
+
+secondary_example_queries =>
+    "info redis";
+
+description "Redis command reference";
+
+name "RedisCommands";
+
+icon_url "/i/redis.io.ico";
+
+source "Redis";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/redis_commands";
+
+topics => "geek", "sysadmin";
+
+category => "reference";
+
+attribution
+    github => ['https://github.com/djworth', 'djworth'],
+    twitter => ['https://twitter.com/djworth', 'djworth'],
+    web => ['http://danworth.com', 'Dan Worth'];
+
+1;
