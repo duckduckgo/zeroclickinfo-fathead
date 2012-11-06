9c74a37847800b3f1b97a64075f94cd081fb90b1
Robert Picard <mail@robert.io>
2012-11-06 14:48:52 -0500

    Add meta information for Java

---
+++ java/Java.pm
@@ -0,0 +1,28 @@
+package DDG::Fathead::Java;
+
+use DDG::Fathead;
+
+primary_example_queries => "java string";
+
+secondary_example_queries =>
+    "integer java";
+
+description "Java reference";
+
+name "Java";
+
+icon_url "/i/download.oracle.com.ico";
+
+source "Oracle";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/java";
+
+topics => "geek", "programming";
+
+category => "reference";
+
+attribution
+    github => ['https://github.com/boyter', 'boyter'],
+    twitter => ['https://twitter.com/boyter', 'boyter'];
+
+1;
