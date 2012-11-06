39d79ec953d574915e6373db3711f1ed8c78cadc
Robert Picard <mail@robert.io>
2012-11-06 16:37:42 -0500

    Add meta information for Plone

---
+++ plone_org/Plone.pm
@@ -0,0 +1,27 @@
+package DDG::Fathead::Plone;
+
+use DDG::Fathead;
+
+primary_example_queries => "plone arecibo";
+
+secondary_example_queries =>
+    "autorole";
+
+description "Plone plugin reference";
+
+name "Plone";
+
+icon_url "/i/plone.org.ico";
+
+source "Plone";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/plone_org";
+
+topics => "geek", "web_design", "special_interest";
+
+category => "reference";
+
+attribution
+    github => ['https://github.com/ezgraphs', 'ezgraphs'];
+
+1;
