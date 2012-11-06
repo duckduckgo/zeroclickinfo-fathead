d76a3c0155f83a8cd91398e86426298b7e3b43f2
Robert Picard <mail@robert.io>
2012-11-06 13:50:07 -0500

    Add meta information for FirefoxAboutConfig

---
+++ firefox_about_config/FirefoxAboutConfig.pm
@@ -0,0 +1,30 @@
+package DDG::Fathead::FirefoxAboutConfig;
+
+use DDG::Fathead;
+
+primary_example_queries => "firefox keyword.url";
+
+secondary_example_queries =>
+    "bidi.numeral",
+    "browser.cache.disk.capacity about:config",
+    "plugin.scan.plid.all firefox config";
+
+description "Firefox about:config information";
+
+name "FirefoxAboutConfig";
+
+icon_url "/i/mozillazine.org.ico";
+
+source "MozillaZine";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/firefox_about_config";
+
+topics => "geek", "sysadmin", "special_interest";
+
+categories => "computing_info", "software";
+
+attribution
+    github => ['https://github.com/nospampleasemam', 'nospampleasemam'],
+    web => ['http://dylansserver.com', 'Dylan Lloyd'];
+
+1;
