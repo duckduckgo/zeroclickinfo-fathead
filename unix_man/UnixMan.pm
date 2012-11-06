b9a9d60b9b9d426b24b3344e5e19470b34b92db9
Robert Picard <mail@robert.io>
2012-11-06 16:11:15 -0500

    Add meta information for UnixMan

---
+++ unix_man/UnixMan.pm
@@ -0,0 +1,28 @@
+package DDG::Fathead::UnixMan;
+
+use DDG::Fathead;
+
+primary_example_queries => "man nmap";
+
+secondary_example_queries =>
+    "wc man";
+
+description "UNIX man page reference";
+
+name "UnixMan";
+
+icon_url "/i/www.linuxcommand.org.ico";
+
+source "LinuxCommand.org";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/unix_man";
+
+topics => "geek", "sysadmin";
+
+category => "software";
+
+attribution
+    github => ['https://github.com/flaming-toast', 'flaming-toast'],
+    web => ['http://ftoast.org', 'Jessica Yu'];
+
+1;
