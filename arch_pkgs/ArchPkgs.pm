452e04989491c1a2500d6b30059dda9707730257
Robert Picard <mail@robert.io>
2012-11-06 13:16:01 -0500

    Add ArchPkgs meta information

---
+++ arch_pkgs/ArchPkgs.pm
@@ -0,0 +1,25 @@
+package DDG::Fathead::;
+
+use DDG::Fathead;
+
+primary_example_queries => "";
+
+secondary_example_queries => "";
+
+description "";
+
+name "";
+
+icon_url "";
+
+source "";
+
+code_url "";
+
+topics => "";
+
+categories => "";
+
+attribution => "";
+
+1;
