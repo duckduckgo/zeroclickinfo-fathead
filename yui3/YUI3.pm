06391da396f78bb9ea8ab84ed59d4abc3af36135
Robert Picard <mail@robert.io>
2012-11-06 16:25:36 -0500

    Add meta information for YUI3

---
+++ yui3/YUI3.pm
@@ -0,0 +1,29 @@
+package DDG::Fathead::YUI3;
+
+use DDG::Fathead;
+
+primary_example_queries => "yui accordion";
+
+secondary_example_queries =>
+    "widget yui";
+
+description "YUI module reference";
+
+name "YUI3";
+
+icon_url "/i/yuilibrary.com.ico";
+
+source "YUI Library";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/yui3";
+
+topics => "geek", "programming", "web_design";
+
+category => "reference";
+
+attribution
+    github => ['https://github.com/gaganpreet', 'gaganpreet'],
+    twitter => ['https://twitter.com/_gagan', '_gagan'],
+    web => ['http://gaganpreet.in', 'Gaganpreet Singh Arora'];
+
+1;
