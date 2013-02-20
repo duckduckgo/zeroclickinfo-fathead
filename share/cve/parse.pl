e961a2c552193ad2ae7ff7e4b0476da57e173b68
Kevin Polulak <soh_cah_toa@cybercrud.net>
2013-02-20 16:00:03 -0500

    Began basic outline of parse.pl script. Simply opens filehandles to input archive and output.txt and then closes them.

--- share/cve/parse.pl
+++ share/cve/parse.pl
@@ -1,1 +1,23 @@
 #!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use IO::Uncompress::AnyInflate;
+use XML::LibXML::Reader;
+
+my $infile  = shift or die '[ERROR] Required input file not given.';
+my $outfile = shift or die '[ERROR] Required output file not given.';
+
+my $infh  = new IO::Uncompress::AnyInflate $infile
+    or die "[ERROR] Failed to open input file: $infile";
+my $outfh = new IO::File;
+
+$outfh->open(">$outfile") or die "[ERROR] Failed to open output file: $outfile";
+$outfh->binmode(':utf8');
+
+my $reader = new XML::LibXML::Reader IO => $infh;
+
+$infh->close();
+$outfh->close();
+$reader->close();
