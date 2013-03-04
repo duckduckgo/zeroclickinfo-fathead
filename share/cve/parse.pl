3148644d52225f7ec5571d19015ea1b2ed3bda36
Kevin Polulak <soh_cah_toa@cybercrud.net>
2013-03-04 14:52:46 -0500

    Fixed issue of rejected and reserved CVE-ID's in output.txt. New parse_entry() subroutine now excludes rejected identifiers from output.txt and includes reversed identifiers but with a clear message about their reserved status.

--- share/cve/parse.pl
+++ share/cve/parse.pl
@@ -25,7 +25,7 @@ trace("Opening output file: $outfile");
 $outfh->open(">$outfile") or die "[ERROR] Cannot open file: $!";
 $outfh->binmode(':utf8');
 
-trace('Creating XML::LibXML::Reader from input archive');
+trace('Creating XML reader from input archive');
 
 my $reader = new XML::LibXML::Reader IO => $infh;
 
@@ -33,12 +33,25 @@ trace('Starting XML tree traversal');
 
 # Parse XML tree as a list of <item> tags, extracting the CVE ID and description.
 while ($reader->nextElement('item') && $reader->readState() != XML_READER_ERROR) {
-    my $name = $reader->getAttribute('name');
-    my $desc = "Vulnerability description: $1" if $reader->readInnerXml =~ m|<desc.*>(.*)</desc>|;
-    my $url  = "http://www.cvedetails.com/cve/$name";
+    my $name      = $reader->getAttribute('name');
+    my $inner_xml = $reader->readInnerXml;
 
-    trace("Adding entry for $name");
+    my ($url, $desc) = parse_entry($name, $inner_xml);
 
+    # Ignore rejected identifiers.
+    if (not (defined $url or defined $desc)) {
+        trace("Ignoring entry for $name (reject)");
+        next;
+    }
+
+    if ($url eq '') {
+        trace("Adding entry for $name (reserved)") if $url eq '';
+    }
+    else {
+        trace("Adding entry for $name");
+    }
+
+    # Add entry to output file.
     print $outfh join "\t", (
         $name,    # Title
         'A',      # Type
@@ -57,9 +70,6 @@ while ($reader->nextElement('item') && $reader->readState() != XML_READER_ERROR)
     );
 }
 
-trace('Finished XML tree traversal');
-trace('Closing filehandles');
-
 $infh->close();
 $outfh->close();
 $reader->close();
@@ -67,6 +77,33 @@ $reader->close();
 my $end_time = time;
 
 trace('Total time: ' . ($end_time - $start_time) . ' seconds');
+
+# Parses the inner XML of a given CVE entry and returns the appropriate URL
+# and descrption.
+#
+# Rejects are removed by returning an undefined URL and description. Reserved
+# identifiers return an empty URL string and a description of their reserved
+# status. Otherwise, the associated URL and description are returned for a
+# valid CVE-ID.
+sub parse_entry {
+    my ($name, $inner_xml) = @_;
+    my $desc = $1 if $inner_xml =~ m|<desc.*>(.*)</desc>|;
+    my $url  = "http://www.cvedetails.com/cve/$name";
+
+    if ($desc =~ /^\*\* REJECT \*\* /) {
+        $desc = undef;
+        $url  = undef;
+    }
+    elsif ($desc =~ /^\*\* RESERVED \*\* (.*)/) {
+        $desc = $1;
+        $url  = '';
+    }
+    else {
+        $desc = "Vulnerability description: $desc";
+    }
+
+    return ($url, $desc);
+}
 
 # For debug purposes only. Displays diagnostic message if $trace is defined.
 sub trace {
