8c425a2a62f3ce0a10e646ad120c2c35ad0f5ed6
Kevin Polulak <soh_cah_toa@cybercrud.net>
2013-03-13 16:29:53 -0400

    Modified parser to ignore reserved CVE-ID's. If it is decided that reserved identifiers should be excluded from the results, then this branch can be merged with master. Otherwise, it can be deleted.

--- share/cve/parse.pl
+++ share/cve/parse.pl
@@ -31,21 +31,17 @@ my $reader = new XML::LibXML::Reader IO => $infh;
 
 trace('Starting XML tree traversal');
 
-# Parse XML tree as a list of <item> tags, extracting the CVE ID and description.
+# Parse XML tree as a list of <item> tags, extracting the CVE-ID and description.
 while ($reader->nextElement('item') && $reader->readState() != XML_READER_ERROR) {
     my $name      = $reader->getAttribute('name');
     my $inner_xml = $reader->readInnerXml;
 
-    my ($url, $desc) = parse_entry($name, $inner_xml);
+    my ($desc, $url) = parse_entry($name, $inner_xml);
 
-    # Ignore rejected identifiers.
+    # Ignore reserved and rejected identifiers.
     if (not (defined $url or defined $desc)) {
-        trace("Ignoring entry for $name (reject)");
+        trace("Ignoring entry for $name (reserved/reject)");
         next;
-    }
-
-    if ($url eq '') {
-        trace("Adding entry for $name (reserved)") if $url eq '';
     }
     else {
         trace("Adding entry for $name");
@@ -81,28 +77,22 @@ trace('Total time: ' . ($end_time - $start_time) . ' seconds');
 # Parses the inner XML of a given CVE entry and returns the appropriate URL
 # and descrption.
 #
-# Rejects are removed by returning an undefined URL and description. Reserved
-# identifiers return an empty URL string and a description of their reserved
-# status. Otherwise, the associated URL and description are returned for a
-# valid CVE-ID.
+# Returns the description and associated URL for a valid CVE-ID. Rejected and
+# reserved identifiers return undef for both the description and URL.
 sub parse_entry {
     my ($name, $inner_xml) = @_;
     my $desc = $1 if $inner_xml =~ m|<desc.*>(.*)</desc>|;
     my $url  = "http://www.cvedetails.com/cve/$name";
 
-    if ($desc =~ /^\*\* REJECT \*\* /) {
+    if ($desc =~ /^\*\* (REJECT|RESERVED) \*\* /) {
         $desc = undef;
         $url  = undef;
-    }
-    elsif ($desc =~ /^\*\* RESERVED \*\* (.*)/) {
-        $desc = $1;
-        $url  = '';
     }
     else {
         $desc = "Vulnerability description: $desc";
     }
 
-    return ($url, $desc);
+    return ($desc, $url);
 }
 
 # For debug purposes only. Displays diagnostic message if $trace is defined.
