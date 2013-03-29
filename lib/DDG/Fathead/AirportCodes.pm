fbf14b258de87a794acc417fa5b7e75fbdf68ec5
Phil <ponzellus@gnuzifer.de>
2013-03-29 18:13:40 +0100

    updated 'source'

--- lib/DDG/Fathead/AirportCodes.pm
+++ lib/DDG/Fathead/AirportCodes.pm
@@ -14,7 +14,7 @@ name "AirportCodes";
 
 icon_url "/i/www.github.com.ico";
 
-source "GitHub";
+source "https://en.wikipedia.org/wiki/List_of_airports_by_IATA_code";
 
 code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/share/airport_codes";
 
