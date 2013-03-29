d28f285744ee0b4f772e5ff67b1dd6938c31cb28
Phil <ponzellus@gnuzifer.de>
2013-03-29 21:37:01 +0100

    fixed title field, now set to IATA code

--- share/airport_codes/parse.pl
+++ share/airport_codes/parse.pl
@@ -18,7 +18,7 @@ my $decdata = decode_json( $json );
 
 foreach my $e_ref ( @{ $decdata } ) {
 	#say $e_ref->{code}." - Airport ".$e_ref->{name}.", ".$e_ref->{location};
-	print $fout "Airport Code\tA\t\t\tAirport Codes\\nIATA\\n\t\t\t\thttps://en.wikipedia.org/wiki/List_of_airports_by_IATA_code Wikipedia Page\t\t\t";
+	print $fout $e_ref->{code}."\tA\t\t\tAirport Codes\\nIATA\\n\t\t\t\thttps://en.wikipedia.org/wiki/List_of_airports_by_IATA_code Wikipedia Page\t\t\t";
 	print $fout $e_ref->{code}." - ".$e_ref->{name}.", ".$e_ref->{location}."\thttps://github.com/jbrooksuk/JSON-Airports/\n";
 }
 
