99a0f478d5ef159a1730bd8da93861c97aaf89c5
Dario Meloni <mellon85@gmail.com>
2013-06-24 16:47:58 +0200

    Added redirections for iternational airports.

--- share/airports/parse.py
+++ share/airports/parse.py
@@ -123,8 +123,12 @@ class Airport(object):
 	def add_redirects(self,output,withRedirect):
 		if self.international_airport_name == None:
 			return
-		abstract = self._format(Airport.name_abstract_format)+self.abstract_icao_part
-		fields = self._getFields(self.international_airport_name,'A',append_period(abstract))
+		abstract   = self._format(Airport.name_abstract_format)+self.abstract_icao_part
+		fields     = self._getFields(self.international_airport_name,'A',append_period(abstract))
+		output.append('%s' % ('\t'.join(fields)))
+		fields	   = self._getFields(self.international_airport_name[0:-len("Airport")-1],'R')
+		fields[2]  = self.international_airport_name
+		fields[12] = ''
 		output.append('%s' % ('\t'.join(fields)))
 
 		if withRedirect:
