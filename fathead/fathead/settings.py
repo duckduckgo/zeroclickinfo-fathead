feef345e25ff9c3ebf56ea55ab3bbfed9c164d3b
Supreet Sethi <supreet.sethi@gmail.com>
2013-10-21 23:33:08 +0530

    Prototype fathead spider written using scrapy.

---
+++ fathead/fathead/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for fathead project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'fathead'
+
+SPIDER_MODULES = ['fathead.spiders']
+NEWSPIDER_MODULE = 'fathead.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'fathead (+http://www.yourdomain.com)'
