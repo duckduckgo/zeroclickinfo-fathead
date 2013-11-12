877e613f82fa2c8f1f94b1735fc1d3e7d4a14daa
koen <koen.metsu@gmail.com>
2013-11-12 22:42:32 +0100

    updated the information file

--- lib/DDG/Fathead/NetFramework.pm
+++ lib/DDG/Fathead/NetFramework.pm
@@ -7,22 +7,22 @@ primary_example_queries "msdn String";
 secondary_example_queries
     "String msdn";
 
-description ".Net Framework class reference";
+description "MSDN .Net Framework class reference";
 
 name "Net framework";
 
-icon_url "dnno";
+icon_url "";
 
 source "MSDN";
 
 code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/share/netframework";
 
-topics "geek", "programming";
+topics "geek", "programming", "dotnet";
 
 category "reference";
 
 attribution
     github => ['https://github.com/koenmetsu', 'koenmetsu'],
     twitter => ['https://twitter.com/koenmetsu', 'koenmetsu'];
-
+	web => ['http://koenmetsu.com', 'koenmetsu'];
 1;
