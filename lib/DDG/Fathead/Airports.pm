600fdf04fbf47b4035cc5451f245c87979446754
Zaahir Moolla <moollaza@fastmail.fm>
2014-01-22 14:10:14 -0500

    Airports - Fixed package name and attribution

--- lib/DDG/Fathead/Airports.pm
+++ lib/DDG/Fathead/Airports.pm
@@ -1,27 +1,17 @@
-package DDG::Fathead::ArchPkgs;
+package DDG::Fathead::Airports;
 
 use DDG::Fathead;
 
 primary_example_queries "iata vie";
-
 secondary_example_queries
     "icao jfk",
     "iata charles du galle";
-
 description "Airport IATA/ICAO codes";
-
 name "Airports";
-
-icon_url "";
-
 source "Wikipedia";
-
 code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/share/airports";
-
 topics "special_interest";
-
-category "aviation";
-
+category "reference";
 attribution
     github => ['https://github.com/mellon85', 'mellon85'];
 
