dd02a72684764113039049fc4a201f58ec3141bc
Zaahir Moolla <moollaza@fastmail.fm>
2014-01-22 13:21:21 -0500

    CVE - Added missing semi-colon

--- lib/DDG/Fathead/CVE.pm
+++ lib/DDG/Fathead/CVE.pm
@@ -3,24 +3,13 @@ package DDG::Fathead::CVE;
 use DDG::Fathead;
 
 primary_example_queries 'cve-2009-1234';
-
-secondary_example_queries
-    'cve 2009-1234';
-
+secondary_example_queries 'cve 2009-1234';
 description 'Common Vulnerabilities and Exposures (CVE) reference';
-
 name 'CVE';
-
-icon_url '';
-
 source 'CVE Details';
-
 code_url 'https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/share/cve';
-
 topics 'computing', 'programming', 'special_interest';
-
 category 'reference';
-
 attribution
     github => ['https://github.com/soh-cah-toa', 'soh-cah-toa'],
     web    => ['http://cybercrud.net', 'Kevin Polulak'];
