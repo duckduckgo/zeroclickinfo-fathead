51a1e399cd3930fb9829da69dbd9a091e6622929
Dylan Lloyd <dylan@dylansserver.com>
2014-02-06 22:46:33 -0500

    cleanup

--- share/sysctl_proc_sys_kernel/parse.pl
+++ share/sysctl_proc_sys_kernel/parse.pl
@@ -6,22 +6,22 @@ use warnings;
 use IO::All;
 
 my $source = 'https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/plain/Documentation/sysctl/kernel.txt';
-open my $output, '>', 'output.txt';
+open my $output, '>', 'output.txt' or die 'Could not open output.txt for writing';
 
-my $documentation = io->file('kernel.txt')->slurp or die 'Could not open ./kernel.txt';
+my $documentation = io->file('kernel.txt')->slurp or die "Could not open ./kernel.txt\nHave you run ./parse.sh?";
 my @sections = split /==============================================================/, $documentation;
 shift @sections for 1..2;
 
 my %answers;
 
 for my $section (@sections) {
-    my $file = (grep {s/://} (split /\n/, $section))[0];
+    my $file = (grep { s/:// } split /\n/, $section)[0];
     next unless $file;
     my $details = $section =~ s/\n\n$file:\n\n|\n*$//gr;
     $details =~ s/\n/ /g;
     $answers{$file} = $details;
     map { $answers{$_} = $details } split / & /, $file if $file =~ /&/;
-    map { chop if /\)$/; $answers{$_} = $details } split / \(/, $file if $file =~ /\(/;
+    map { s/\)$//; $answers{$_} = $details } split / \(/, $file if $file =~ /\(/;
 }
 
 %answers = map { lc, $answers{$_} } keys %answers;
