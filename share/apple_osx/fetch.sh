aa5e48e14ce527b09b370c7d34715da430dde158
Ben Boyter <bboyte01@gmail.com>
2014-06-19 07:40:08 +1000

    Change the fetch option slightly.

--- share/apple_osx/fetch.sh
+++ share/apple_osx/fetch.sh
@@ -4,4 +4,5 @@ cd $(dirname -- "$0")
 
 # wget madness here... this takes a long time
 # may want to consider --limit-rate=20k to avoid being blocked
-wget -m -c --remote-encoding=UTF-8 --wait 0.25 https://developer.apple.com/library/mac/sitemap.php
+# sadly you cannot --no-clobber with -m
+wget -m  --remote-encoding=UTF-8 --wait 0.25 https://developer.apple.com/library/mac/sitemap.php
