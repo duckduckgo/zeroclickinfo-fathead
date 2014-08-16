785271fdd1a5a804168ba08557827998332e4f40
Yanir Seroussi <yanir.seroussi@gmail.com>
2014-08-16 14:26:17 +1000

    pypi: extended parse.py to include number of package downloads, release date and development status

--- share/pypi/README.md
+++ share/pypi/README.md
@@ -13,5 +13,3 @@ PyPI package name and description fetcher and parser.
   https://pypi.python.org/pypi/Django/json works while https://pypi.python.org/pypi/django/json doesn't, even though
   https://pypi.python.org/pypi/django (without '/json') correctly redirects to https://pypi.python.org/pypi/Django.
   This is a known issue: https://bitbucket.org/pypa/pypi/issue/23/package-redirect-behaviour-inconsistent
-* The package JSONs contain more information that may be worth including in the search result. Notably, the description
-  field might be useful to users.
--- share/pypi/parse.py
+++ share/pypi/parse.py
@@ -15,22 +15,35 @@ with codecs.open('download/package-jsons', encoding='utf-8') as in_file, \
         codecs.open('output.txt', mode='wb', encoding='utf-8') as out_file:
     for package_json in in_file:
         package_dict = json.loads(package_json)
-        release_url = quote_url(package_dict['info']['release_url'])
+        package_info = package_dict['info']
+
+        # Extract external links
+        release_url = quote_url(package_info['release_url'])
         external_links = '[%s Release page]' % release_url
-        home_page = package_dict['info'].get('home_page', None)
+        home_page = package_info.get('home_page', None)
         if home_page and home_page.startswith('http'):
             try:
                 external_links += '\\\\n[%s Official site]' % quote_url(home_page)
             except KeyError:  # Can happen for Chinese URLs -- we can live without them (only one found)
                 pass
 
-        summary = package_dict['info']['summary']
+        # Build abstract
+        abstract_lines = []
+        summary = package_info['summary']
         if not summary or summary == 'UNKNOWN':
             continue
-        summary = re.sub(r'\s', ' ', summary, flags=re.MULTILINE | re.UNICODE)
+        abstract_lines.append(re.sub(r'\s', ' ', summary, flags=re.MULTILINE | re.UNICODE))
+        abstract_lines.append('Downloads in the last month: %s' % package_info['downloads']['last_month'])
+        latest_release_info = package_dict['releases'][package_info['version']]
+        if latest_release_info:
+            abstract_lines.append('Release date: %s' % latest_release_info[0]['upload_time'].split('T')[0])
+        for classifier in package_info['classifiers']:
+            if classifier.startswith('Development Status'):
+                abstract_lines.append('Development status: %s' % classifier.split(' - ')[-1])
+                break
 
         out_file.write('\t'.join([
-            package_dict['info']['name'],  # Title
+            package_info['name'],  # Title
             'A',  # Article type
             '',  # No redirect
             '',  # Other uses (ignored)
@@ -41,7 +54,7 @@ with codecs.open('download/package-jsons', encoding='utf-8') as in_file, \
             external_links,
             '',  # Disambiguation (ignored)
             '',  # No images
-            summary,  # Abstract
+            '<br>'.join(abstract_lines),
             release_url,  # Source url
         ]))
         out_file.write('\n')
