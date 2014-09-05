4dd3cbb6d47d57c993cea45dfc7ba618df48316e
elebow <elebow@users.noreply.github.com>
2014-09-05 16:16:09 -0400

    Jargon: Use more realistic example queries.

--- lib/DDG/Fathead/Jargon.pm
+++ lib/DDG/Fathead/Jargon.pm
@@ -2,9 +2,9 @@ package DDG::Fathead::Jargon;
 
 use DDG::Fathead;
 
-primary_example_queries "jargon creeping elegance";
+primary_example_queries "creeping elegance";
 secondary_example_queries
-    "jargon one-line fix", "jargon dinosaur";
+    "one-line fix", "dinosaur";
 description "Jargon File glossary lookup";
 name "Jargon";
 source "The Jargon File (http://catb.org/~esr/jargon/html/index.html)";
