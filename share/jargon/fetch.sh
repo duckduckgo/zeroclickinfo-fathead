7176e7639edd2236390236bc9d4c6a67661dc11a
elebow <elebow@users.noreply.github.com>
2014-09-19 15:21:56 -0400

    Jargon: Don't extract unneeded files from tarball.

--- share/jargon/fetch.sh
+++ share/jargon/fetch.sh
@@ -9,4 +9,4 @@ if [ ! -e download/jargsrc.tar.gz ]; then
     wget "http://catb.org/~esr/jargon/jargsrc.tar.gz" -O download/jargsrc.tar.gz
 fi
 
-tar xzf download/jargsrc.tar.gz --exclude=docbook-xsl-\* -C download/
+tar xzf download/jargsrc.tar.gz --exclude=docbook-xsl-\* --exclude=graphics --exclude=\*html --exclude=\*xsl --exclude=READ.ME --exclude=Makefile --exclude=chaff.xml --exclude=jargon-mode.el --exclude=jargon.css --exclude=BUGS -C download/
