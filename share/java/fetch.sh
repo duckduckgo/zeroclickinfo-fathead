779de030cfb65cfac61d4fceca4451651c3ec8eb
Carsten <carsten@kopis.de>
2014-11-22 10:27:09 +0100

    Apply fathead rules
    After reading https://duck.co/duckduckhack/fathead_overview I had to
    modify fetch.sh and re-add data.url.

--- share/java/fetch.sh
+++ share/java/fetch.sh
@@ -1,3 +1,5 @@
 # see http://stackoverflow.com/a/10959815/834
-wget --quiet --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-docs-all.zip
-unzip -q jdk-8u25-docs-all.zip
+rm -rf download
+mkdir -p download
+wget --quiet --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O download/jdk-8u25-docs-all.zip http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-docs-all.zip
+unzip -q download/jdk-8u25-docs-all.zip
