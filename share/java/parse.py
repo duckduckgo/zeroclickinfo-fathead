c1e95e0415e1feb1c0f98331bbe0147d8b285b87
Carsten <carsten@kopis.de>
2014-11-23 07:37:25 +0100

    Generate full output.txt
    I populate all necessary fields, following
    https://duck.co/duckduckhack/fathead_overview for reference. The link to
    JDK URL is hardcoded, but it works and shows the full frame version.

--- share/java/parse.py
+++ share/java/parse.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import os
-import re
 from bs4 import BeautifulSoup
 import sys
 import string
@@ -21,10 +20,16 @@ def getDocs(filename):
   if filename.endswith('.html') and 'package-' not in filename and 'doc-files' not in filename:
     content = BeautifulSoup(getcontent(filename))
     classname = content.find_all('h2')[0].string
-    description = content.find_all('div', 'description', limit=1)[0].get_text()
-    return classname, description
+    block = content.find_all('div', 'block', limit=1)
+    if len(block) > 0:
+      description = block[0].get_text()
+      description = description[0:description.find('.', 100) + 1].replace("\n", "")
+    else:
+      description = 'no description found'
+    url = "https://docs.oracle.com/javase/8/docs/api/index.html?" + filename.replace("./docs/api/", "")
+    return classname, description, url
   else:
-    return ""
+    return ["", "", ""]
 
 def getcontent(filename):
   f = open(filename, 'r')
@@ -32,16 +37,43 @@ def getcontent(filename):
   f.close()
   return lines
 
-def output(filename, (clazz, description)):
+def concat_list(data_list = ['', '', '']):
+  return concat(data_list[0], data_list[1], data_list[2])
+
+def concat(clazz, description, url):
+  title = clazz or 'No class found'
+  typez = 'A'
+  redirect = ''
+  four = ''
+  categories = ''
+  six = ''
+  related_topics = '' # [[Perl Data Language|PDL]], can be multiples?
+  eight = ''
+  external_links = '' # [$url title text]\\n, can be multiples
+  ten = ''
+  image = ''
+  abstract = description.replace("\n", "\\n").replace("\t", "\\t") or "No abstract found"
+  url = url or "No URL found"
+  
+  data = [title, typez, redirect, four, categories, six, related_topics, eight, external_links, ten, image, abstract, url]
+  line = "\t".join(data) + "\n"
+  return line
+
+def output(filename, data_list):
   f = open(filename, 'a')
-  line = clazz + "\t" + description.replace("\n", "\\n").replace("\t", "\\t")
-  line += "\n"
+  line = concat_list(data_list)
+
   f.write(line.encode('utf'))
   f.close()
 
-files = []
-files.append(collectDocFilesFrom('./docs/api/java'))
-files.append(collectDocFilesFrom('./docs/api/javax'))
 
-output("output.txt", getDocs('./docs/api/java/lang/String.html'))
+# delete previous output
+os.remove('output.txt')
+# iterate package "java"
+for f in collectDocFilesFrom('./docs/api/java'):
+  output("output.txt", getDocs(f))
+
+# iterate package "javax"
+for f in collectDocFilesFrom('./docs/api/javax'):
+  output("output.txt", getDocs(f))
 
