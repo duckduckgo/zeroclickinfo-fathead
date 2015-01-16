250963253d58d0e63ebb108a20fd237a3459244b
jdorweiler <jddorweiler@gmail.com>
2015-01-16 16:28:57 -0500

    revert parse.sh

--- share/mdnjs/parse.sh
+++ share/mdnjs/parse.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-#rm output.txt
-
 python parse.py --out output.txt \
                 --langdefs ecma.csv \
                 --cachedir downloads \
                 --cachejournal ".cachejournal"
-
-#python parse.py --out output.js.txt \
-#                --langdefs ecma.csv \
-#                --cachedir downloads_js \
-#                --cachejournal ".cachejournal"
-
-#cat output.api.txt >> output.txt
-#cat output.js.txt >> output.txt
