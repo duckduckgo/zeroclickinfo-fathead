8ab135dfcd6457e595787de96fc5640b5f8b211d
jdorweiler <jddorweiler@gmail.com>
2015-01-16 16:27:18 -0500

    comment

--- share/mdnjs/parse.sh
+++ share/mdnjs/parse.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-rm output.txt
+#rm output.txt
 
-python parse.py --out output.api.txt \
+python parse.py --out output.txt \
                 --langdefs ecma.csv \
                 --cachedir downloads \
                 --cachejournal ".cachejournal"
 
-python parse.py --out output.js.txt \
-                --langdefs ecma.csv \
-                --cachedir downloads_js \
-                --cachejournal ".cachejournal"
+#python parse.py --out output.js.txt \
+#                --langdefs ecma.csv \
+#                --cachedir downloads_js \
+#                --cachejournal ".cachejournal"
 
-cat output.api.txt >> output.txt
-cat output.js.txt >> output.txt
+#cat output.api.txt >> output.txt
+#cat output.js.txt >> output.txt
