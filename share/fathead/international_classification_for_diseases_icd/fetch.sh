378dfaa5feb41395ad1d320e9054594ef6cc4f18
mkorvink <mkorvink@gmail.com>
2016-01-29 02:50:33 +0000

    updates to support procedures and diagnoses as well as icd code redirects for those that include decimals

--- share/fathead/international_classification_for_diseases_icd/fetch.sh
+++ share/fathead/international_classification_for_diseases_icd/fetch.sh
@@ -4,4 +4,6 @@ wget -P download -N 'https://www.cms.gov/Medicare/Coding/ICD10/Downloads/2016-Co
 
 unzip -o download/2016-Code-Descriptions-in-Tabular-Order.zip -d download
 
+wget -P download -N 'www.cdc.gov/nchs/data/icd/icd10cm/2016/ICD10CM_FY2016_code_descriptions.zip';
 
+unzip -o download/ICD10CM_FY2016_code_descriptions.zip -d download
--- share/fathead/international_classification_for_diseases_icd/parse.py
+++ share/fathead/international_classification_for_diseases_icd/parse.py
@@ -1,47 +1,92 @@
 url = "https://www.cms.gov/Medicare/Coding/ICD10/"
-title = "International Classification of Diseases 10 Clinical Modification Code (ICD-10 CM)"
+title = "International Classification of Diseases Code (ICD-10)"
 article_type = "A"
 
 outp = "output.txt"
-inp = "download/icd10cm_codes_2016.txt"  #reference the ICD file here
+cm_inp = "download/icd10cm_codes_2016.txt"  # reference the ICD file here
+pcs_inp = "download/icd10pcs_codes_2016.txt"
 
-#Open input file
-input_file = open(inp, "r" )
+# Open input files
+cm_input_file = open(cm_inp, "r")
+pcs_input_file = open(pcs_inp, "r")
 
-#Read and throw out first line
-input_file.readline()
+# Open output file
+output_file = open(outp, "w")
 
-output_file = open( outp, "w")
+# Read and throw out first line
+cm_input_file.readline()
 
-#Loop thru the remainder of the file, format each line
-#and print it to the output file.
-for line in input_file.readlines() :
-	line = line.strip();
-	pair = line.split( '   ' );
-	if len(pair ) < 2 :
-		continue;
+# Loop through the remainder of the file, format each line
+# and print it to the output file.
+for line in cm_input_file.readlines():
 
-        pair[0] = unicode(pair[0])
+    line = line.strip();
+    icd_cm_code = line[0:7]
+    if len(icd_cm_code.strip()) > 3:
+            icd_cm_alt_code = icd_cm_code[0:3]+"."+icd_cm_code[3:7]
+    icd_cm_desc = line[8:200]
+    print icd_cm_code
+    print icd_cm_alt_code
+    print icd_cm_desc
 
-        abstract = "ICD-10 Description: " + pair[1]
 
-        output_file.write( "\t".join([
-            pair[0],        # Title
-            article_type,   # Type
-            '',             # Redirect
-            '',             # Other uses
-            '',             # Categories
-            '',             # References
-            '',             # See also
-            '',             # Further reading
-            '',             # External links
-            '',             # Disambiguation
-            '',             # Images
-            abstract,       # Abstract
-            url,            # Source URL
-            ] ))
+    icd_cm_code = unicode(icd_cm_code)
 
-        output_file.write( "\n" );
+    abstract = "ICD-10 CM Description: " + icd_cm_desc
 
-input_file.close();
+    output_file.write("\t".join([
+        icd_cm_code,  # Title
+        article_type,  # Type
+        'R',  # Redirect
+        icd_cm_alt_code,  # Other uses
+        '',  # Categories
+        '',  # References
+        '',  # See also
+        '',  # Further reading
+        '',  # External links
+        '',  # Disambiguation
+        '',  # Images
+        abstract,  # Abstract
+        url,  # Source URL
+    ]))
+
+    output_file.write("\n");
+
+    # Read and throw out first line
+pcs_input_file.readline()
+
+# Loop through the remainder of the file, format each line
+# and print it to the output file.
+for line in pcs_input_file.readlines():
+    line = line.strip();
+    icd_pcs_code = line[0:7]
+    icd_pcs_desc = line[8:200]
+    print line[0:7]
+    print line[8:200]
+
+    icd_pcs_code = unicode(icd_pcs_code)
+
+    abstract = "ICD-10 Description: " + icd_pcs_desc
+
+    output_file.write("\t".join([
+        icd_pcs_code,  # Title
+        article_type,  # Type
+        '',  # Redirect
+        '',  # Other uses
+        '',  # Categories
+        '',  # References
+        '',  # See also
+        '',  # Further reading
+        '',  # External links
+        '',  # Disambiguation
+        '',  # Images
+        abstract,  # Abstract
+        url,  # Source URL
+    ]))
+
+    output_file.write("\n");
+
+
+cm_input_file.close();
+pcs_input_file.close();
 output_file.close();
