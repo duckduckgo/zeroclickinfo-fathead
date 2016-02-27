d783c1eecbc384683a5a3c6b473c217a959c4bbf
samskeller <samskeller@gmail.com>
2016-02-27 16:51:55 +0000

    Adding Perl file that lists metadata about this Fathead

---
+++ lib/DDG/Fathead/GitManual.pm
@@ -0,0 +1,22 @@
+package DDG::Fathead::GitManual;
+
+use DDG::Fathead;
+
+primary_example_queries "git commit";
+
+description "Git commands";
+
+name "GitManual";
+
+source "Git Documentation";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-fathead/tree/master/share/fathead/git_manual";
+
+topics "version_control", "programming";
+
+category "reference";
+
+attribution
+    github => ['https://github.com/samskeller', 'samskeller'];
+
+1;
