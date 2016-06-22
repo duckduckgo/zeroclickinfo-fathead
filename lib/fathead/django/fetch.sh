b532acf5db348097ab823922207b8e373f6490c2
Robert Gaul <robert.gaul@ithaka.org>
2016-06-22 11:54:20 -0400

    Remove semi-colons

--- lib/fathead/django/fetch.sh
+++ lib/fathead/django/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p download;
-cd download;
-rm -f *.html;
+mkdir -p download
+cd download
+rm -f *.html
 wget --quiet https://docs.djangoproject.com/en/1.9/ref/templates/builtins/
