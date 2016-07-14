be40253cdfdb698c0b3509f7a5196ec6f07b2bf8
John Barrett <john@jbrt.org>
2016-07-14 13:17:14 +0100

    Adding tsv generating utilities + Moo-ifying parse.pl

--- lib/fathead/perl_doc/Parsers/parse.pl
+++ lib/fathead/perl_doc/Parsers/parse.pl
@@ -1,20 +1,60 @@
 #!/usr/bin/env perl
+
 use strict;
 use warnings;
+
 binmode STDOUT, ":utf8";
+
 use IO::All;
 use Mojo::DOM;
-use Data::Dumper;
-use Term::ProgressBar;
 use Cwd qw( getcwd );
 use Util qw( get_row trim_abstract);
+use Moo;
+use File::Spec;
+use DBI;
+use Text::CSV_XS;
 
-my @pages = glob(getcwd(). "/../download/internals/*.html");
-@pages = (@pages, 
-    glob(getcwd(). "/../download/language/*.html"),
-    glob(getcwd(). "/../download/pragmas/*.html"),
-    glob(getcwd(). "/../download/utilities/*.html"),
-);
+has working_dir => ( is => 'lazy' );
+sub _build_working_dir {
+    getcwd;
+}
+
+has docs_dir => ( is => 'lazy' );
+sub _build_docs_dir {
+    File::Spec->catdir( $_[0]->working_dir, qw/ .. download perldoc-html / );
+}
+
+has indices => ( is => 'lazy' );
+sub _build_indices {
+    
+}
+
+has tsv => ( is => 'lazy' );
+sub _build_tsv {
+    my $dbh = DBI->connect ("dbi:CSV:", undef, undef, {
+        csv_sep_char     => "\t",
+        csv_class        => "Text::CSV_XS",
+    });
+
+    $dbh->do ( sprintf ( "CREATE TABLE output.txt (%s)",
+        join ( ', ', map { "$_ CHAR" } ( qw/
+            title type alias null1 categories null2 related null3
+            links disambiguation image abstract sourceurl
+        / ) )
+    ) );
+
+    return $dbh;
+}
+
+sub insert {
+    my ( $self, $data ) = @_;
+    my @keys = keys $data;
+    my $sql = sprintf( "INSERT INTO output.txt (%s) VALUES (%s)",
+        join( ", ", @keys ),
+        join( ", ", map { '?' } @keys ),
+    );
+    $self->tsv->do( $sql, undef, @{ $data }{ @keys } );
+}
 
 foreach my $page (@pages){
     my $html < io($page);
@@ -64,3 +104,11 @@ foreach my $page (@pages){
 
        printf("%s\n", get_row($title, $description, "http://perldoc.perl.org/$page", 'A'));
 }
+
+sub test_csv {
+    my ( $self ) = @_;
+    $self->insert({ title => 'asd', sourceurl => 'bsd' });
+}
+
+main->new->test_csv;
+
