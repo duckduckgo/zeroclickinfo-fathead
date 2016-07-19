c6e90f98bd9b83de6c748612c3e26364639bbdd8
Ben Moon <guiltydolphin@gmail.com>
2016-07-19 19:25:26 +0100

    Ensure no unnecessary work is done in parsing

--- lib/fathead/perl_doc/parse.sh
+++ lib/fathead/perl_doc/parse.sh
@@ -1,7 +1,3 @@
-# cleanup download dir first
-find download/functions/ -type f -not -name "*functions*.html" -exec rm {} \;
-cd Parsers
-rm output.txt
-perl parse.pl >> output.txt 
-perl parse-functions.pl >> output.txt
-mv output.txt ../
+cd Parsers && perl parse.pl > output.txt \
+  && perl parse-functions.pl >> output.txt \
+  && mv output.txt ..
