4bb6cd76e9ff34da91e9a8667e78fa00acc2c232
Ben Moon <guiltydolphin@gmail.com>
2016-07-19 18:35:55 +0100

    Ensure correct initialization of download files

--- lib/fathead/perl_doc/fetch.sh
+++ lib/fathead/perl_doc/fetch.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cd download
-wget http://perldoc.perl.org/perldoc-html.tar.gz
-tar xf perldoc-html.tar.gz
-
+mkdir -p download && cd download \
+  && wget http://perldoc.perl.org/perldoc-html.tar.gz \
+  && tar xf perldoc-html.tar.gz
