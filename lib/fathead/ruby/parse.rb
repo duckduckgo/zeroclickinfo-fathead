d4607e18d7924b93489211cd1cb05c0654b413de
David Jones <david@djones.eu>
2016-07-29 11:50:52 +0100

    Cleanup

--- lib/fathead/ruby/parse.rb
+++ lib/fathead/ruby/parse.rb
@@ -19,36 +19,27 @@ class OutputRow
     :url
   ].freeze
 
-  ARTICLE_TYPES = {
-    article: 'A',
-    disambiguation: 'D',
-    redirect: 'R'
-  }.freeze
-
-  attr_reader(*HEADERS.compact - [:abstract])
-  attr_writer(*HEADERS.compact - [:type])
+  attr_accessor(*HEADERS.compact)
 
   def initialize
     yield self
     freeze
   end
 
-  def type=(value)
-    @type = ARTICLE_TYPES.fetch(value)
-  end
-
-  def abstract
-    @abstract.to_s.gsub("\n", '\n')
-  end
-
   def to_s
-    HEADERS.map { |header| public_send header if header } * "\t" << "\n"
+    HEADERS.map { |header| escape public_send header if header } * "\t" << "\n"
+  end
+
+  private
+
+  def escape(text)
+    String(text).gsub("\n", '\n').gsub("\t", '\t')
   end
 end
 
 # Abstract description of documentation.
 class Docs
-  attr_accessor :name, :url, :description
+  attr_accessor :title, :url, :description
 
   def initialize
     yield self
@@ -56,24 +47,38 @@ class Docs
   end
 
   def abstract
-    intro = description.css('p,pre.ruby,h2').take_while { |e| e.name != 'h2' }
     intro.chunk(&:name).map do |name, elements|
       if name == 'pre'
-        elements.map { |e| "<pre><code>#{e.text}</code></pre>" }.join
+        elements.map { |e| code_block e.text }.join
       else
-        elements.map { |e| e.text.gsub(/\s+/, ' ') }.join('<br>')
+        elements.map { |e| squish e.text }.join('<br>')
       end
     end.join
   end
 
   def to_row
     OutputRow.new do |row|
-      row.title = name
-      row.type = :article
+      row.title = title
+      row.type = 'A' # article
       row.categories = categories
       row.abstract = abstract
       row.url = url
     end
+  end
+
+  private
+
+  def intro
+    description.css('p,pre.ruby,h2').take_while { |e| e.name != 'h2' }
+  end
+
+  def code_block(text_or_lines)
+    code = Array(text_or_lines).join("\n")
+    code.empty? ? '' : "<pre><code>#{code}</code></pre>"
+  end
+
+  def squish(text)
+    String(text).gsub(/[[:space:]]+/, ' ').strip
   end
 
   # Class or module documentation.
@@ -81,14 +86,14 @@ class Docs
     attr_accessor :type
 
     def categories
-      type.to_s == 'module' ? 'modules' : 'classes'
+      String(type) == 'module' ? 'modules' : 'classes'
     end
   end
 
   # Class method or instance method documentation.
   class Method < Docs
     def categories
-      "#{name.to_s.include?('#') ? 'instance' : 'class'} methods"
+      "#{String(title).include?('#') ? 'instance' : 'class'} methods"
     end
 
     def abstract
@@ -98,8 +103,7 @@ class Docs
     private
 
     def usage
-      text = description.css('.method-callseq').map(&:text).join("\n")
-      text.nil? || text.empty? ? '' : "<pre><code>#{text}</code></pre>"
+      code_block description.css('.method-callseq').map(&:text)
     end
   end
 end
@@ -117,13 +121,15 @@ if $PROGRAM_NAME == __FILE__
 
   base_dir = Pathname(ARGF.path).dirname
   base_url = URI('http://ruby-doc.org/core/')
+  index_page = Nokogiri(ARGF)
 
-  Nokogiri(ARGF).css('#class-index .entries p:not(.nodoc)').each do |entry|
+  index_page.css('#class-index .entries p:not(.nodoc)').each do |entry|
     rel_path = entry.at('a')['href']
-    page = Nokogiri(base_dir.join(rel_path).read)
+    path = base_dir + rel_path
+    page = Nokogiri(path.read)
 
     class_docs = Docs::Class.new do |docs|
-      docs.name = page.at('h1').text
+      docs.title = page.at('h1').text
       docs.url = base_url + rel_path
       docs.type = entry['class']
       docs.description = page.at('#description')
@@ -133,7 +139,7 @@ if $PROGRAM_NAME == __FILE__
 
     page.css('#method-list-section .link-list a').each do |link|
       Docs::Method.new do |docs|
-        docs.name = class_docs.name + link.text
+        docs.title = class_docs.title + link.text
         docs.url = class_docs.url + link['href']
         docs.description = page.at(":has([name=#{docs.url.fragment}])")
 
