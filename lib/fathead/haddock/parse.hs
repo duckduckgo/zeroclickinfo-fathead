32d434b619b710387cffb21fea505cfdf4b0ca59
Ben Moon <guiltydolphin@gmail.com>
2016-08-11 15:51:08 +0100

    Move utility functions into separate module

--- lib/fathead/haddock/parse.hs
+++ lib/fathead/haddock/parse.hs
@@ -4,49 +4,9 @@ import Data.Char (isAlpha)
 import Text.XML.HXT.Core
 import Data.Monoid ((<>))
 import Data.Tree.NTree.TypeDefs (NTree)
-import Network.URI (URI, parseURI)
-import Data.Maybe (fromJust)
 
 import FatHead.DB
-
-
-pagePath :: String -> FilePath
-pagePath = (basePath<>)
-  where basePath = "download/haddock/doc/html/"
-
-
-readHaddockDocument :: String -> IOStateArrow s b XmlTree
-readHaddockDocument = readDocument sysConfig . pagePath
-  where sysConfig = [withInputEncoding iso8859_1, withParseHTML yes]
-
-
-hasClass :: ArrowXml a => String -> a XmlTree XmlTree
-hasClass c = hasAttrValue "class" (==c)
-
-
-buildAbstract :: ArrowXml a => a b XmlTree -> a b String
-buildAbstract p = (eelem "span" += p >>> normalizeText >>> writeDocumentToString [withOutputHTML, withRemoveWS yes])
-                  >. (makeAbstract . concat)
-  where makeAbstract = id
-
-
-normalizeText :: ArrowXml a => a XmlTree XmlTree
-normalizeText = processTopDown $ choiceA [ hasName "p" :-> normalizeP
-                                         , hasName "pre" :-> normalizePre
-                                         , this :-> this]
-  where normalizeP = processChildren (changeText (unwords . lines) `when` isText)
-        normalizePre = processChildren (changeText (escapeNewlines . stringTrim) `when` isText)
-        escapeNewlines = concatMap (\x -> if x == '\n' then "\\n" else [x])
-
-
-makeSourceLink :: (Arrow a) => String -> a String URI
-makeSourceLink page = arr (base<>) >>> arr parseURIWithBase
-  where base = "http://www.haskell.org/haddock/doc/html/" <> page <> "#"
-        parseURIWithBase = maybe (fromJust $ parseURI base) id . parseURI
-
-
-withClass :: ArrowXml cat => String -> String -> cat XmlTree XmlTree
-withClass n c = hasName n >>> hasClass c
+import FatHead.Parse
 
 
 parseSections :: Int -> String -> String -> IO [Entry]
@@ -67,28 +27,6 @@ parseDefinitions = parseSections 2 "h3"
 
 parseSectionsTop :: String -> IO [Entry]
 parseSectionsTop = parseSections 1 "h2"
-
-
-onDl :: (ArrowXml a, ArrowList a) => a XmlTree b -> a XmlTree b' -> a XmlTree [(b, b')]
-onDl f g = definitionList >>> unlistA >>> listA (f *** g)
-
-
-definitionList :: (ArrowXml a, ArrowList a) => a XmlTree [(XmlTree, XmlTree)]
-definitionList = listA (getChildren >>> (dt <+> dd)) >>> partitionA dt >>> arr pairs
-  where pairs = uncurry zip
-        dt = hasName "dt"
-        dd = hasName "dd"
-
-
-defaultAbstract :: IOSLA (XIOState ()) (NTree XNode) String
-defaultAbstract = buildAbstract isAbstract
-  where isAbstract = getChildren >>> (hasName "p") <+> (hasName "pre")
-
-
-sourceLink :: String -> IOSLA (XIOState ()) XmlTree URI
-sourceLink page = hasName "a"
-                  >>> getAttrValue "name"
-                  >>> makeSourceLink page
 
 
 parseModuleAttributes :: IO [Entry]
