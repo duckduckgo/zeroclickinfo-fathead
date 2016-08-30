e529845c8f49850b6406f71b0f895a3e7e01a10d
Ben Moon <guiltydolphin@gmail.com>
2016-08-30 21:50:34 +0100

    Add support for caching DOMs

--- lib/fathead/gnome_doc/Util/Page.pm
+++ lib/fathead/gnome_doc/Util/Page.pm
@@ -27,14 +27,16 @@ sub _build_dom {
     my ($self) = @_;
     my $url = $self->url;
     if (my $path = $self->_local_path) {
-        warn "No such file '$path'" and return unless $path->exists;
-        my $dom = Mojo::DOM->new($path->slurp_utf8)
-            or warn "Unable to parse DOM for '$path'\n" and return;
-        return $dom;
+        _dom_from_file($path);
     } else {
+        if (my $dom = $self->_retrieve_from_cache) {
+            return $dom;
+        }
         my $res = $ua->get($url)->success
             or warn "Unable to fetch DOM for '$url'\n";
-        return $res->dom if $res;
+        my $dom = $res->dom if $res;
+        $self->_cache($dom) if $dom;
+        return $dom;
     }
 }
 
@@ -42,6 +44,34 @@ sub _local_path {
     my ($self) = @_;
     return unless my ($path) = $self->url =~ q{^file://(.+)$};
     return path($path);
+}
+
+sub _url_cache_path {
+    my ($url) = @_;
+    return path('download/.cache', Mojo::URL->new($url)->path);
+}
+
+sub _cache {
+    my ($self, $dom) = @_;
+    my $path = _url_cache_path($self->url);
+    $path->touchpath;
+    $path->spew_utf8($dom->to_string);
+}
+
+sub _retrieve_from_cache {
+    my ($self) = @_;
+    my $path = _url_cache_path($self->url);
+    return unless $path->exists;
+    _dom_from_file($path)
+        or $path->remove; # Maybe it'll correct itself in a future parse.
+}
+
+sub _dom_from_file {
+    my ($path) = @_;
+    warn "No such file '$path'" and return unless $path->exists;
+    my $dom = Mojo::DOM->new($path->slurp_utf8);
+    warn "Unable to parse DOM for '$path'\n" and return unless $dom->[0];
+    return $dom;
 }
 
 1;
