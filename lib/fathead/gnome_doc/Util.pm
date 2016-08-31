d6ca1fe51cea78cc8ec34bf9e44e744b789c5e79
Ben Moon <guiltydolphin@gmail.com>
2016-08-31 14:37:15 +0100

    Add helpers for creating parsers and indexers

--- lib/fathead/gnome_doc/Util.pm
+++ lib/fathead/gnome_doc/Util.pm
@@ -6,7 +6,7 @@ use Util::Disambiguation;
 BEGIN {
     require Exporter;
     our @ISA = qw(Exporter);
-    our @EXPORT_OK = qw(article disambiguation);
+    our @EXPORT_OK = qw(article disambiguation indexer parser);
 }
 
 sub article {
@@ -15,6 +15,14 @@ sub article {
 
 sub disambiguation {
     Util::Disambiguation->new(@_);
+}
+
+sub indexer {
+    Util::Index->new(@_);
+}
+
+sub parser {
+    Util::Parser->new(@_);
 }
 
 1;
