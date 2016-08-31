ca15763722f23ec7082c15d14067429e9602dba0
Ben Moon <guiltydolphin@gmail.com>
2016-08-31 14:37:54 +0100

    Add parser for Gtk functions

--- lib/fathead/gnome_doc/parse.pl
+++ lib/fathead/gnome_doc/parse.pl
@@ -3,4 +3,38 @@
 use strict;
 use warnings;
 
-use Util;
+use Util qw(article disambiguation indexer parser);
+
+my $gnome_gtk = 'https://developer.gnome.org/gtk3/stable';
+
+my $gtk_api_symbol_index = indexer(
+    index_url => "$gnome_gtk/api-index-full.html",
+    index_function => sub {
+        my ($dom) = @_;
+        $dom->find('div.index a.link[title]:last-child')->map(attr => 'href')->to_array;
+    },
+    assign_parsers => sub { [\&gtk_api_parse_functions] },
+);
+
+sub gtk_api_parse_functions {
+    my ($dom) = @_;
+    my $fnd = $dom->at('div.refsect1 > a[name$="functions_details"]')->parent;
+    my @articles;
+    foreach my $fn ($fnd->find('div.refsect2')->each) {
+        push @articles, article(
+            anchor => $fn->at('a[name]')->attr('name'),
+            title  => $fn->at('h3')->text =~ s/ \(\)//r,
+            abstract => $fn->find('h3 ~ *')->join(),
+        );
+    }
+    return {
+        articles => \@articles,
+        disambiguations => [],
+    };
+}
+
+my $parser = parser(
+    indexers  => [$gtk_api_symbol_index],
+);
+
+$parser->parse;
