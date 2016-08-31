d99c85e32d1f8bfdde7bbf18eaa9141e192b22c3
Ben Moon <guiltydolphin@gmail.com>
2016-08-31 12:30:23 +0100

    Add helper for creating disambiguation pages

--- lib/fathead/gnome_doc/Util.pm
+++ lib/fathead/gnome_doc/Util.pm
@@ -1,15 +1,20 @@
 package Util;
 
 use Util::Article;
+use Util::Disambiguation;
 
 BEGIN {
     require Exporter;
     our @ISA = qw(Exporter);
-    our @EXPORT_OK = qw(article);
+    our @EXPORT_OK = qw(article disambiguation);
 }
 
 sub article {
     Util::Article->new(@_);
+}
+
+sub disambiguation {
+    Util::Disambiguation->new(@_);
 }
 
 #######################################################################
