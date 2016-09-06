b0e15f304ad1e5d82474140bb159303419a41afa
John Barrett <john@jbrt.org>
2016-09-06 07:16:48 -0400

    Reverting 6fed2b1 to restore index-language
    This contains articles for For, Foreach and more

--- lib/fathead/perl_doc/Parsers/parse.pl
+++ lib/fathead/perl_doc/Parsers/parse.pl
@@ -155,6 +155,7 @@ sub doc_fullurl {
 # Parsers for other keys (basenames) will only run on the matching file.
 my %parser_map = (
     'index-faq'       => ['parse_faq'],
+    'index-language'  => ['parse_faq'],
     'index-functions' => ['parse_functions'],
     'perldiag'        => ['parse_diag_messages'],
     'perlglossary'    => ['parse_glossary_definitions'],
