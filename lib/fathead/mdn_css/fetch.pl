33e44a159d499e929b9aa99005f5544afa4f29ac
rpohankar <rasikapohankar@gmail.com>
2016-09-07 18:30:18 +0000

    Add fetching of webkit and mozilla extensions

--- lib/fathead/mdn_css/fetch.pl
+++ lib/fathead/mdn_css/fetch.pl
@@ -17,113 +17,121 @@ fetch.sh script since it is more efficient to fetch and parse the DOM using
 something other than BASH to ease development and maintenance in the future.
 =cut
 
+my @urls_to_fetch = ('https://developer.mozilla.org/en-US/docs/Web/CSS/Reference', 
+                     'https://developer.mozilla.org/en-US/docs/Web/CSS/Mozilla_Extensions',
+                     'https://developer.mozilla.org/en-US/docs/Web/CSS/Webkit_Extensions');
+
 my $ua = Mojo::UserAgent->new()->max_redirects(4);
-my $reference_url =
-  Mojo::URL->new('https://developer.mozilla.org/en-US/docs/Web/CSS/Reference');
-my $tx = $ua->get($reference_url);
 
 my %urls;    #hash used to remove duplicate urls
 
-if ( $tx->success ) {
-
-=begin
-  We extract the collection of links to keywords from the DOM. The links wanted
-  are found from this part of the DOM:
-  <div class="index">
-    <span>A</span>
-    <ul>
-      <li>
-        <a href="/en-US/docs/Web/CSS/:active"><code>:active</code></a>
-      </li>
-    </ul>
-    ...
-  </div>
-=cut
-
-    my $divs = $tx->res->dom->find('div.index, div.column-half');
-    for my $div ( $divs->each ) {
-        for my $ul ( $div->find('ul')->each ) {
-            $ul->find('li')->map(
-                sub {
-                    my $li = shift;
-                    my $relative_url =
-                      Mojo::URL->new( $li->at('a')->attr('href') );
-                    my $absolute_url = $relative_url->to_abs( $tx->req->url );
-                    $urls{$absolute_url} = 1;
-                }
-            );
-        }
-    }
-}
-elsif ( my $error = $tx->error ) {
-    croak sprintf "Error: %d %s while fetching %s", $error->{code} || 0,
-      $error->{message},
-      $tx->req->url;
-}
-
-#downloaded file names will be named 1.html, 2.html ....
-my $file_number = 1;
-
-my $current_active_connections = 0;
-my $maximum_active_connections = 4;
+my $file_number = 1,
+   $current_active_connections = 0,
+   $maximum_active_connections = 4;
 
 my @keyword_urls;
 
-=begin
-    save the urls with fragments to a text file called
-    fragments.txt so that parse.pl can use this information
-    to extract extra information about the fragments
-=cut
 
-open(
-    my $fragments_fh,   '>:encoding(UTF-8)',
-    catfile 'download', 'fragments.txt'
-) or die $!;
-
-for my $url ( keys %urls ) {
-    $url = Mojo::URL->new($url);
-    if ( $url->fragment ) {
+for my $url (@urls_to_fetch) {
+    my $reference_url = Mojo::URL->new($url);
+    my $tx = $ua->get($reference_url);
+    print "fetching url $url\n";
+    fetch($tx);
+    print "downloading $url\n";
+    download();
+}
 
 =begin
-    For fragment in urls past this path /transform-function/ we can get
-    the link for the actual fragment in the following way.
-    Take for example this fragment #matrix()
-    In order to get the actual link, take the fragment, matrix(), remove
-    the brackets, and append it to the original path so that we get the link
-    https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/matrix
+      We extract the collection of links to keywords from the DOM. The links wanted
+      are found from this part of the DOM:
+      <div class="index">
+        <span>A</span>
+        <ul>
+          <li>
+            <a href="/en-US/docs/Web/CSS/:active"><code>:active</code></a>
+          </li>
+        </ul>
+        ...
+      </div>
 =cut
 
-        if ( $url =~ qr/transform-function/ ) {
-            my $clone =
-              Mojo::URL->new( sprintf "%s://%s", $url->protocol, $url->host );
+sub fetch {
+    my $tx = shift;
 
-            #trailing / needed at the end so that when we add a
-            #fragment at the end it does not replace the former last
-            #part of path
-            $clone->path( $url->path . '/' );
-            my $fragment = $url->fragment;
-            $fragment =~ s/\(\)//g;
-            $clone->path($fragment);
-            push @keyword_urls, $clone;
-        }
-        elsif ( $url->fragment =~ /The_url/ ) {
-
-            #It is this: https://developer.mozilla.org/en-U/docs/Web/CSS/
-            #/url#The_url()_functional_notation
-            #We get rid of the fragment part so that it can be downloaded
-            #and parsed by parse.pl because its format is like the others
-            my $clone =
-              Mojo::URL->new( sprintf "%s://%s", $url->protocol, $url->host );
-            $clone->path( $url->path );
-            push @keyword_urls, $clone;
-        }
-        else {
-            #we will deal with other types of urls later in parse.pl
-            say $fragments_fh $url;
+    if ( $tx->success ) {
+      my $divs = $tx->res->dom->find('div.index, div.column-half');
+        for my $div ( $divs->each ) {
+            for my $ul ( $div->find('ul')->each ) {
+                $ul->find('li')->map(
+                    sub {
+                        my $li = shift;
+                        print "\n value of li $li";
+                        if($li->at('a')) {
+                            my $relative_url = Mojo::URL->new( $li->at('a')->attr('href') );
+                            my $absolute_url = $relative_url->to_abs( $tx->req->url );
+                            $urls{$absolute_url} = 1;
+                        }
+                    }
+                );
+            }
         }
     }
-    else {
-        push @keyword_urls, $url;
+    elsif ( my $error = $tx->error ) {
+        croak sprintf "Error: %d %s while fetching %s", $error->{code} || 0,
+          $error->{message},
+          $tx->req->url;
+    }
+}
+
+sub download {
+    open(
+        my $fragments_fh,   '>:encoding(UTF-8)',
+        catfile 'download', 'fragments.txt'
+    ) or die $!;
+
+    for my $url ( keys %urls ) {
+        $url = Mojo::URL->new($url);
+        if ( $url->fragment ) {
+
+#     For fragment in urls past this path /transform-function/ we can get
+#     the link for the actual fragment in the following way.
+#     Take for example this fragment #matrix()
+#     In order to get the actual link, take the fragment, matrix(), remove
+#     the brackets, and append it to the original path so that we get the link
+#     https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/matrix
+
+            if ( $url =~ qr/transform-function/ ) {
+                my $clone =
+                  Mojo::URL->new( sprintf "%s://%s", $url->protocol, $url->host );
+
+                #trailing / needed at the end so that when we add a
+                #fragment at the end it does not replace the former last
+                #part of path
+                $clone->path( $url->path . '/' );
+                my $fragment = $url->fragment;
+                $fragment =~ s/\(\)//g;
+                $clone->path($fragment);
+                push @keyword_urls, $clone;
+            }
+            elsif ( $url->fragment =~ /The_url/ ) {
+
+                #It is this: https://developer.mozilla.org/en-U/docs/Web/CSS/
+                #/url#The_url()_functional_notation
+                #We get rid of the fragment part so that it can be downloaded
+                #and parsed by parse.pl because its format is like the others
+                my $clone =
+                  Mojo::URL->new( sprintf "%s://%s", $url->protocol, $url->host );
+                $clone->path( $url->path );
+                push @keyword_urls, $clone;
+            }
+            else {
+                #we will deal with other types of urls later in parse.pl
+                say $fragments_fh $url;
+            }
+        }
+        else {
+            push @keyword_urls, $url;
+        }
     }
 }
 
