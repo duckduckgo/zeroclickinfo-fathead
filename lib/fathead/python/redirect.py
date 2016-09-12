caf78f5c4217ae1e416d6be31124502cd5bb0d74
Sourav Badami <souravbadami@gmail.com>
2016-09-12 15:14:39 +0530

    Minor changes

--- lib/fathead/python/redirect.py
+++ lib/fathead/python/redirect.py
@@ -206,7 +206,7 @@ def generate_redirects(f):
                         else:
                             # New entry of disambiguation type
                             redirect.set_entry_type('D')
-                            output[key] = str(redirect.get_key())
+                            output[key] = str(redirect.get_key()) + '\t' + 'D'
 
                         duplicate_count += 1
 
