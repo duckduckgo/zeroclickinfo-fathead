e1741bcb605996df94059e7e49c09df978408291
Harrison Chienjo <hhathersage@gmail.com>
2016-09-27 09:10:43 +0300

    Explicitly specify parser.

--- lib/fathead/python_requests/parse.py
+++ lib/fathead/python_requests/parse.py
@@ -5,6 +5,6 @@ from glob import glob
 
 for html_file in glob('download/*.html'):
     print("Processing %s" % html_file)
-    soup = BeautifulSoup(open(html_file))
+    soup = BeautifulSoup(open(html_file), "html.parser")
     page_url = soup.find('link', attrs={'rel': 'canonical'}).get('href')
     print("Page url %s" % page_url)
