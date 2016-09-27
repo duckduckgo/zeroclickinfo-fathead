52de0c752e1f8d2c40d6b9c59aa4ff6bf0ebba43
Harrison Chienjo <hhathersage@gmail.com>
2016-09-27 09:09:13 +0300

    Extract page url.

--- lib/fathead/python_requests/parse.py
+++ lib/fathead/python_requests/parse.py
@@ -6,3 +6,5 @@ from glob import glob
 for html_file in glob('download/*.html'):
     print("Processing %s" % html_file)
     soup = BeautifulSoup(open(html_file))
+    page_url = soup.find('link', attrs={'rel': 'canonical'}).get('href')
+    print("Page url %s" % page_url)
