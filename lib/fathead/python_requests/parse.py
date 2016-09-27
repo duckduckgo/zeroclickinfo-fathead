5f44b2c8b60bc5319f3bff896cd130ec268e222f
Harrison Chienjo <hhathersage@gmail.com>
2016-09-27 18:08:09 +0300

    Open output.txt.

--- lib/fathead/python_requests/parse.py
+++ lib/fathead/python_requests/parse.py
@@ -42,11 +42,12 @@ def build_article(h2_parent, page_url):
     }
     return outputline
 
-for html_file in glob('download/*.html'):
-    print("Processing %s" % html_file)
-    soup = BeautifulSoup(open(html_file), "html.parser")
-    page_url = soup.find('link', attrs={'rel': 'canonical'}).get('href')
-    print("Page url %s" % page_url)
-    h2s = soup.findAll('h2')
-    for h2 in h2s:
-        build_article(h2.parent, page_url)
+with open('output.txt', 'w') as fp:
+    for html_file in glob('download/*.html'):
+        print("Processing %s" % html_file)
+        soup = BeautifulSoup(open(html_file), "html.parser")
+        page_url = soup.find('link', attrs={'rel': 'canonical'}).get('href')
+        print("Page url %s" % page_url)
+        h2s = soup.findAll('h2')
+        for h2 in h2s:
+            output = build_article(h2.parent, page_url)
