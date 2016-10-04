4b2ed193170320759768352d37a610852b936479
Harrison Chienjo <hhathersage@gmail.com>
2016-10-04 14:04:36 +0300

    Add download authentication url.

--- lib/fathead/python_requests/fetch.sh
+++ lib/fathead/python_requests/fetch.sh
@@ -5,3 +5,4 @@ mkdir -p download
 cd download
 wget http://docs.python-requests.org/en/master/user/quickstart/
 wget -O advanced.html  http://docs.python-requests.org/en/master/user/advanced/
+wget -O authentication.html http://docs.python-requests.org/en/master/user/authentication/
