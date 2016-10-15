e28cc165f8372ea22edd3cc89fc6e65ad9bec09a
rpohankar <rasikapohankar@gmail.com>
2016-10-15 14:55:30 +0000

    Update regex which matches pseudo classes, use different variable for title with spaces so the redirect matches

--- lib/fathead/mdn_css/parse.pl
+++ lib/fathead/mdn_css/parse.pl
@@ -296,16 +296,19 @@ sub create_redirects {
 
     my $title = shift;
     my $title_clean;
+    my $title_with_space;
     my $lookup = _category_title($title);
     my @data;
     my $postfix;
     my $outputline;
 
     # Add a space between the pseudo class and parentheses, when it is not present
-    if($title =~ /(:?:?-moz.+)[^\s]\((.+)\)/) {
-        $title = "$1 ($2)";
+    if($title =~ /(:?:?-moz[^\s\(]+)\((.+)\)/) {
+        $title_with_space = "$1 ($2)";
+        $title_clean = _clean_string($title_with_space);
+    } else {
+        $title_clean = _clean_string($title);
     }
-    $title_clean = _clean_string($title);
 
     if (exists $titles{$lookup}) {
         #TODO If multiple categories per article exist, improve redirect creation
