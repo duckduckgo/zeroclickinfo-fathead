65532e4d2b8fdf060f22ac85f0df79441533590c
Rasika Pohankar <rasikapohankar@gmail.com>
2016-10-20 02:36:41 +0530

    MDN CSS: Add spaces where parentheses are removed (#394) (#400)
    * Add space where brackets are removed in _clean_string
    
    * Add output.txt
    
    * Add a space between pseudo class and parentheses, undo adding space in _clean_string
    
    * Add new output.txt
    
    * Update regex which matches pseudo classes, use different variable for title with spaces so the redirect matches
    
    * Add new output.txt

--- lib/fathead/mdn_css/parse.pl
+++ lib/fathead/mdn_css/parse.pl
@@ -295,11 +295,20 @@ sub create_redirects {
 =cut
 
     my $title = shift;
-    my $title_clean = _clean_string($title);
+    my $title_clean;
+    my $title_with_space;
     my $lookup = _category_title($title);
     my @data;
     my $postfix;
     my $outputline;
+
+    # Add a space between the pseudo class and parentheses, when it is not present
+    if($title =~ /(:?:?-moz[^\s\(]+)\((.+)\)/) {
+        $title_with_space = "$1 ($2)";
+        $title_clean = _clean_string($title_with_space);
+    } else {
+        $title_clean = _clean_string($title);
+    }
 
     if (exists $titles{$lookup}) {
         #TODO If multiple categories per article exist, improve redirect creation
