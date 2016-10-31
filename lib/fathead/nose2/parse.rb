a91b0601e07b64a436fd6c0115efe61b84d6e8a2
rnkaufman <nina@procore.com>
2016-10-30 23:32:16 -0700

    Regex for removing classes, added output.txt for what works currently

--- lib/fathead/nose2/parse.rb
+++ lib/fathead/nose2/parse.rb
@@ -10,7 +10,8 @@ topics.each do |topic|
   url = "http://nose2.readthedocs.io/en/latest/getting_started.html##{topic}"
   doc = Nokogiri::HTML(open(url))
   abstract = doc.css("##{topic}").css("p").to_s
-  output_row = [topic, 'A', '', '', '', '', '', '', NOSE_HOME, '', '', abstract, url]
+  abstract = abstract.gsub("\n", " ").gsub(/class\=\S{1,2}[a-z]*(\s[a-z]*)?\S{1,2}/, "")
+  output_row = [topic, 'A', '', '', '', '', '', '', NOSE_HOME, '', '', abstract, url].join("\t")
 
-  File.open('output.txt', 'a') { |f| f.write(output_row) }
+  File.open('output.txt', 'a') { |f| f.write(output_row + "\n") }
 end
