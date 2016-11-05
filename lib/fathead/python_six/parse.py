3283a39098d5315a99a6c9a453701cb330b07549
iamrajhans <rajhans.jadhao@gmail.com>
2016-11-06 01:46:10 +0530

    added class to parse the python six library content and added output.txt file

--- lib/fathead/python_six/parse.py
+++ lib/fathead/python_six/parse.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
-import glob 
+import glob
+import sys
 
+python_six_base_url = 'https://pythonhosted.org/six/'
 
-class Command(object):
+class SixModule(object):
     '''
     Class for storing all necessary information about the python six lib. This
     information is then used to create the output.txt file using the
@@ -23,10 +26,14 @@ class Command(object):
 
         # Clean up the usage statement which can have newline characters and
         # tab characters, which mess up how it renders
-        usage_cleaned = self.usage.replace('\n', '\\n').replace('\t', '    ')
+#        print self.usage
+
+        usage_cleaned = self.usage.replace("’","").replace("\t", "    ").replace("\n","\\n")
+        
+#        print usage_cleaned
 
         # Make the abstract have the description as well as a code block
-        # for the usage of the command
+        
         abstract = '{}\n<pre><code>{}</pre></code>'.format(self.description,
                                                            usage_cleaned)
         return '\t'.join([
@@ -45,3 +52,50 @@ class Command(object):
             '{}/{}'.format(python_six_base_url,
 			   self.filename),  # URL
 	])
+
+class Parser(object):
+    def __init__(self):
+        '''Get all files that need to be parsed'''
+        self.files_to_parse = glob.glob('download/*.html')
+
+    def parse_module(self):
+        '''Parse module and make a SixModule object for each'''
+        self.six_module = []
+
+        for file in self.files_to_parse:
+            soup = BeautifulSoup(open(file), 'html.parser')
+            div = soup.find('div',{'class':'section'})
+        
+            if not div :
+                continue
+        
+            for tag in div.find_all('dl'):
+            
+                '''Get the module name '''
+                module_name = tag.dt.select('code')[1].get_text(strip=True) if tag.dt.select('code')[1].get_text(strip=True) != 'six.' else tag.dt.select('code')[2].get_text(strip=True)
+            
+                '''Get the module desc '''
+                description = tag.dd.p.getText()
+            
+                '''Get code for module if present '''
+                code = tag.dd.div.getText() if tag.dd.div else ''
+
+                # Create the SixModule object
+                module = SixModule(module_name, description,file.replace('download/', ''))
+                module.usage = code
+                #Add the module in SixModule
+                self.six_module.append(module)
+
+if __name__ == '__main__':
+
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    #Create the Parser
+    parser = Parser()
+
+    parser.parse_module()
+
+    # Write the output in Output.txt
+    with open('output.txt', 'w+') as output:
+        for module in parser.six_module:
+            output.write((module.__str__() + '\n'))
