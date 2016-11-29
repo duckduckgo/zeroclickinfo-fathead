bc6da7b8bbbf423615dcd0c31c02abf97fa14db8
Maria Grazia Alastra <maria@duckduckgo.com>
2016-11-29 16:36:25 -0500

    write list of articles to a new file

--- lib/utils/extract_articles.pl
+++ lib/utils/extract_articles.pl
@@ -5,16 +5,18 @@
 use strict;
 use warnings;
 use Text::CSV;
+use FindBin;
 
 binmode STDOUT, ":utf8";
 binmode STDERR, ":utf8";
 
-my $ia = $ARGV[0];
-my $path = "../fathead/" . $ia . "/";
+my $ia = $ARGV[0] or die "Please specify an IA";
+my $path = $FindBin::Dir . "/../fathead/" . $ia . "/";
 my $csv = Text::CSV->new({sep_char => "\t"});
-my $file = $path . 'Output.txt';
-open(my $fh, '<:encoding(UTF-8)', $file) 
-    or die "Could not open $file: $!";
+my $file = $path . 'output.txt';
+
+# Read article titles from Output.txt
+open my $fh, '<:encoding(UTF-8)', $file or die "Could not open $file: $!";
 
 my @titles;
 
@@ -22,3 +24,12 @@ while( my $line = $csv->getline($fh))  {
     $file->[1] =~ /A/ or next;  #skip unless it's an article
     push @titles, $file->[0]; #first field is the title
 }
+close $fh;
+
+# Write list of articles to the new file
+open my $fw, '>:encoding(UTF-8)', $path . "articles.txt" or die();
+
+foreach my $title (@titles) {
+    print $fw $title . "\n";
+}
+close $fw;
