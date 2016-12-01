a5be014029a8b6e3ac48ca03a936f5c89097cd50
nikhilsingh291 <nikhilsingh291@gmail.com>
2016-12-01 23:38:21 +0530

    almost done

---
+++ lib/fathead/cpp_boost/fetch.sh
@@ -0,0 +1,6 @@
+
+
+mkdir -p download
+cd download
+rm -f *.html
+wget http://www.boost.org/doc/libs/1_62_0
--- lib/fathead/cpp_boost/parse.py
+++ lib/fathead/cpp_boost/parse.py
@@ -1,7 +1,8 @@
 import os
 from bs4 import BeautifulSoup
 URL_ROOT = 'http://www.boost.org'
-URL_DOC = ''
+
+URL_DOC = 'http://www.boost.org/doc/libs/1_62_0/'
 
 
 DOWNLOADED_HTML_PATH = 'download/'
@@ -29,10 +30,15 @@ class BoostDataParser:
 
     def __init__(self,raw_data):
 
-        #self.parsed_data=None
+        self.parsed_data=None
         
         self.soup_data=BeautifulSoup(raw_data,'html.parser')
         self.doc_content=self.soup_data.find('div',{'class':'section-body'})
+
+
+        self.filter_data(self.doc_content)
+
+        
 ##        for tag in soup.find_all('dl',{'class':'fields'}):
 ##            gop=tag.findAll('dt')
 ##            for gopa in gop:
@@ -83,24 +89,49 @@ class BoostDataParser:
     def get_data(self):
         return self.parsed_data
             
+
+class BoostDataOutput:
+
+    def __init__(self,data):
+
+        self.data = data
+
+    def write_file(self):
+
+
+         with open('output.txt', 'w+') as output_file:
+            for data_element in self.data:
+                if data_element.get('name'):
+                    name = data_element.get('name')
+                    first_paragraph = '<p>' + data_element.get('first_paragraph') + '</p>'
+                    abstract = first_paragraph
+                    abstract = '<section class="prog__container">' + abstract + '</section>'
+                    url = '{}'.format(data_element.get('anchor'))
+                    list_of_data = [
+                        name,       # 1. article title
+                        'A',        # 2.type is article
+                        '',         # 3.no redirect data
+                        '',         # 4.leave empty
+                        '',         # 5.no categories
+                        '',         # 6.leave empty
+                        '',         # 7.no related topics
+                        '',         # 8.leave empty
+                        URL_DOC,    # 9.an external link back to home
+                        '',         # 10.no disambiguation
+                        '',         # 11.images
+                        abstract,   # 12.abstract
+                        url         # 13.url to doc
+                    ]
+
+                    output_file.write('{}\n'.format('\t'.join(list_of_data)))
+
+
         
-        
-
-
-
-
-
-
 if __name__=="__main__":
     
     boost_data=BoostData()
     raw_data=boost_data.get_raw_data()
-    #print(raw_data)
     boost_parse=BoostDataParser(raw_data)
-    #print(boost_parse.doc_content)
-    boost_parse.filter_data(boost_parse.doc_content)
     boost_parse.parse_final_data()
-    #print(boost_parse.parse_name_and_anchor(boost_parse.filtered_doc))
-    #print(boost_parse.parse_first_paragraph(boost_parse.filtered_doc))
-    
-    print(boost_parse.get_data())
+    output=BoostDataOutput(boost_parse.get_data())
+    output.write_file()
