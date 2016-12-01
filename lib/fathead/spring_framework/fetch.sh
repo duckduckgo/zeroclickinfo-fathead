4b363a22d2eeddaf95f52e875e2dc675db10bdba
Dimitrios Mandekis <dimitrios@ingenioussolutions.org>
2016-12-01 19:49:38 +0100

    Java: Create New Spring Framework Fathead #554

--- lib/fathead/spring_framework/fetch.sh
+++ lib/fathead/spring_framework/fetch.sh
@@ -5,12 +5,13 @@ rm -rf docs
 mkdir -p download
 mkdir -p docs
 cd ./download/
-unamestr=`uname`
-if [[ "$unamestr" == 'Darwin' ]]; then
-   curl -O http://docs.spring.io/spring/docs/current/javadoc-api/overview-summary.html
-else
+# This is for OSX support.  In OSX wget is not installed by default but curl is.  Uncomment it if you need to run it on OSX.
+#unamestr=`uname`
+#if [[ "$unamestr" == 'Darwin' ]]; then
+#   curl -O http://docs.spring.io/spring/docs/current/javadoc-api/overview-summary.html
+#else
   wget --quiet --no-check-certificate --no-cookies -O overview-summary.html http://docs.spring.io/spring/docs/current/javadoc-api/overview-summary.html
-fi
+#fi
 cd ..
 python ./fetch.py
 unzip -q ./download/*.zip -d ./docs/
