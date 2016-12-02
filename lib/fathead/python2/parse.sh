ad243c26039cfad7534da9b5ee747c3b095273a4
Robert Nilsson <robert.nilsson85@gmail.com>
2016-12-02 07:54:30 +0000

    Run redirect.py on the python2 results to generate permutations for results.
    This also removes the duplicates that are generated from parsing.

--- lib/fathead/python2/parse.sh
+++ lib/fathead/python2/parse.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 (cd ../python; ./parse.sh)
-cp ../python/output_py2.txt output.txt
+#run redirect on python2 results to get permutations of redirects, and remove
+# duplicates
+cp ../python/output_py2.txt ../python/output.txt
+(cd ../python; python redirect.py)
+cp ../python/output2.txt output.txt
