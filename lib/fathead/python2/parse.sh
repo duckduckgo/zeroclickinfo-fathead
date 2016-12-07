84cf14ba0174cec1d2d547e3d0e01a84b257dc5f
Robert Nilsson <robert.nilsson85@gmail.com>
2016-12-07 10:35:41 +0000

    Clarify output from parse.sh and add new output.txt

--- lib/fathead/python2/parse.sh
+++ lib/fathead/python2/parse.sh
@@ -2,9 +2,10 @@
 
 export PYTHONIOENCODING=utf8
 (cd ../python; ./parse.sh)
-#run redirect on python2 results to get permutations of redirects, and remove
+#Run redirect on python2 results to get permutations of redirects, and remove
 # duplicates
 cp ../python/output_py2.txt ../python/output.txt
 (cd ../python; python redirect.py)
 cp ../python/output2.txt output.txt
+echo "Python 2 output contains"
 cat output.txt | ../python/output_statistics.awk
