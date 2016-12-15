9cce798ef7d1d45a3bbaac3ea1d33a1f4e80369a
Ashish Bhate <bhate.ashish@gmail.com>
2016-12-15 17:24:18 +0530

    Numpy: Remove space and newline at end of line.

--- lib/fathead/numpy/parse.py
+++ lib/fathead/numpy/parse.py
@@ -255,7 +255,7 @@ def get_params(section_div, params_class):
         except AttributeError:
             pass
 
-    return ''.join(parameters)
+    return ''.join(parameters).rstrip()
 
 
 def get_related_topics(section_div):
