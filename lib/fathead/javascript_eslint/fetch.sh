67ace0fbb25b3f8067995a89c68458232a360532
zodiac 403 <zodiac403@gmx.de>
2017-01-13 20:10:00 +0000

    Test fetch + parse - Add output.txt - Tune download folder structure.

--- lib/fathead/javascript_eslint/fetch.sh
+++ lib/fathead/javascript_eslint/fetch.sh
@@ -5,4 +5,5 @@ rm -R download
 mkdir -p download
 
 # download
+mkdir -p download/summary
 curl https://raw.githubusercontent.com/eslint/eslint.github.io/master/_data/rules.yml -o ./download/summary/rules.yml
