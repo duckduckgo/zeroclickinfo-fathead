26eb7f98ebd0772ff29057a3f3d401d0a6a38efa
itai vaks <itai421@gmail.com>
2017-01-19 14:14:40 +0000

    a working fetch script

--- lib/fathead/webpack_config/fetch.sh
+++ lib/fathead/webpack_config/fetch.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
-mkdir -p downloads
+ouput_dir='downloads';
+
+mkdir -p $ouput_dir;
+
+#getting all the links from  a tags with class code-link
+./fetch/fetch.js > tempLinks;
+
+#removin the part after #, removing duplicate links and getting only /configuration links
+awk -F '#' '{print $1}' tempLinks | sort | uniq | grep configuration > links;
+
+#fetching all the links and saving them to output_dir
+wget -i links -P "./$ouput_dir/" 
+
+#cleanup
+rm -f tempLinks links;
 
