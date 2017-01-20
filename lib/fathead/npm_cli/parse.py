10d24e3f11602b00353d6ce18debed5c34d9198b
Rahul Arora <rahulxxarora@gmail.com>
2017-01-20 13:54:08 +0000

    Final changes

--- lib/fathead/npm_cli/parse.py
+++ lib/fathead/npm_cli/parse.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
-START_URL = 'https://docs.npmjs.com/cli/access'
+START_URL = 'https://docs.npmjs.com/cli/install'
 BASE_URL = 'https://docs.npmjs.com'
 OUTPUL_FILE = open('output.txt', 'w+')
 RESULT_URLS = []
 
 
 # Print the content to the output file
-def print_line(title, code, content):
+def print_article_line(title, code, content):
 
+    title = 'npm-' + title
     code = code.replace('\n', '\\n').replace('\t', '  ')
     code = code.replace('<', '&lt;').replace('>', '&gt;')
     content = content.replace('\n', '\\n').replace('\t', '  ')
@@ -41,6 +43,139 @@ def print_line(title, code, content):
     OUTPUL_FILE.write('{}\n'.format('\t'.join(list_of_data)))
 
 
+# Print the redirect info to the output file
+def print_redirect_line(title, code):
+
+    # Redirect
+    new_title = 'npm-' + title
+    command_dict = {}
+
+    # Always add the original title
+    list_of_data = [
+        title,            # 1. article title
+        'R',              # 2.type is article
+        new_title,        # 3.no redirect data
+        '',               # 4.leave empty
+        '',               # 5.no categories
+        '',               # 6.leave empty
+        '',               # 7.no related topics
+        '',               # 8.leave empty
+        '',               # 9.an external link back to home
+        '',               # 10.no disambiguation
+        '',               # 11.images
+        '',               # 12.abstract
+        ''                # 13.url to doc
+        ]
+
+    OUTPUL_FILE.write('{}\n'.format('\t'.join(list_of_data)))
+
+    for i in code.split('\n'):
+        # Check explicitly for alias and aliases, both follow diff pattern
+        if i.startswith('aliases'):
+            temp = str(i.split('aliases: ')[-1])
+            temp = temp.split(', ')
+
+            for alias in temp:
+                list_of_data = [
+                    alias,            # 1. article title
+                    'R',              # 2.type is article
+                    new_title,        # 3.no redirect data
+                    '',               # 4.leave empty
+                    '',               # 5.no categories
+                    '',               # 6.leave empty
+                    '',               # 7.no related topics
+                    '',               # 8.leave empty
+                    '',               # 9.an external link back to home
+                    '',               # 10.no disambiguation
+                    '',               # 11.images
+                    '',               # 12.abstract
+                    ''                # 13.url to doc
+                    ]
+
+                OUTPUL_FILE.write('{}\n'.format('\t'.join(list_of_data)))
+
+            continue
+
+        if i.startswith('alias'):
+            command = i.split('npm ')[-1]
+
+            list_of_data = [
+                command,          # 1. article title
+                'R',              # 2.type is article
+                new_title,        # 3.no redirect data
+                '',               # 4.leave empty
+                '',               # 5.no categories
+                '',               # 6.leave empty
+                '',               # 7.no related topics
+                '',               # 8.leave empty
+                '',               # 9.an external link back to home
+                '',               # 10.no disambiguation
+                '',               # 11.images
+                '',               # 12.abstract
+                ''                # 13.url to doc
+            ]
+
+            OUTPUL_FILE.write('{}\n'.format('\t'.join(list_of_data)))
+
+            continue
+
+        # Other irrelevant info
+        if not i or not i.startswith('npm'):
+            continue
+
+        command = re.split(r' [<[()]', i)[0].replace('npm ', '')
+        temp = re.findall(r'[^a-z](-+[a-z]+)', i)
+
+        if not temp and command not in command_dict:
+            command_dict[command] = True
+
+            list_of_data = [
+                command,          # 1. article title
+                'R',              # 2.type is article
+                new_title,        # 3.no redirect data
+                '',               # 4.leave empty
+                '',               # 5.no categories
+                '',               # 6.leave empty
+                '',               # 7.no related topics
+                '',               # 8.leave empty
+                '',               # 9.an external link back to home
+                '',               # 10.no disambiguation
+                '',               # 11.images
+                '',               # 12.abstract
+                ''                # 13.url to doc
+            ]
+
+            OUTPUL_FILE.write('{}\n'.format('\t'.join(list_of_data)))
+
+            continue
+
+        for option in temp:
+            final_command = command + ' ' + option
+
+            if final_command in command_dict:
+                continue
+
+            command_dict[final_command] = True
+
+            list_of_data = [
+                final_command,    # 1. article title
+                'R',              # 2.type is article
+                new_title,        # 3.no redirect data
+                '',               # 4.leave empty
+                '',               # 5.no categories
+                '',               # 6.leave empty
+                '',               # 7.no related topics
+                '',               # 8.leave empty
+                '',               # 9.an external link back to home
+                '',               # 10.no disambiguation
+                '',               # 11.images
+                '',               # 12.abstract
+                ''                # 13.url to doc
+            ]
+
+            OUTPUL_FILE.write('{}\n'.format('\t'.join(list_of_data)))
+
+
 # Extracts content of all the pages
 def get_func_desc():
 
@@ -55,7 +190,8 @@ def get_func_desc():
         info = ob.findAll('p')
         content = info[0].text
 
-        print_line(title, code, content)
+        print_article_line(title, code, content)
+        print_redirect_line(title, code)
 
 
 # Extracts links to all pages from Home Page
