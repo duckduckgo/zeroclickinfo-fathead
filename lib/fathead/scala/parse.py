a7c0dc5c2a306ebad8a7a76a7c73393774a29f64
Sam Radhakrishnan <sk09idm@gmail.com>
2017-01-21 11:03:53 +0000

    Initial Commit
    Add a new Scala Documentation fathead. Resolves https://github.com/duckduckgo/zeroclickinfo-fathead/issues/692

---
+++ lib/fathead/scala/parse.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import os
+
+BASE_DIRECTORY = "./download/api/scala"
+BASE_URL = "https://www.scala-lang.org/api/current/"
+
+
+def get_all_files():
+	doc_files = []
+
+	for (path, dirs, files) in os.walk(BASE_DIRECTORY):
+		for f in files:
+			doc_files.append( "%s/%s" % (path, f))
+	return doc_files
+
+def get_docs(filename):
+	if filename[-5:] == ".html":
+		soup = BeautifulSoup(open(filename), 'html.parser')
+		title_link = soup.find_all("h1")[0]
+		title_text = title_link.text
+		
+		url = BASE_URL + filename.replace("./download/api/", "") #title_link.find("a").get('href')
+
+		article_text = soup.find_all("div", attrs={"class" : "comment"})[0].text
+		
+		return title_text, url, article_text
+
+def get_tuple(filename):
+	title , url ,abstract = get_docs(filename)
+	data = ['']*13
+	data[0] = title
+	data[1] = 'A'
+	data[12] = url
+	if abstract == None:
+		abstract = "No data found"
+	abstract = abstract.replace("\n", "\\n").replace("\t","\\t")
+	abstract = '<section class="prog_container">' + abstract + '</section>'
+	data[11] = abstract
+	return data
+
+def output(filename="output.txt"):
+	doc_files = get_all_files()
+	line = ''
+	i = 0
+	for doc_file in doc_files:
+		data = get_tuple( doc_file )
+		line += "\t".join(data) + "\n"
+		i+=1
+		if i == 100:
+			break
+	f = open(filename, 'a')
+	f.write(line.encode('utf'))
+	f.close()
+
+output()
