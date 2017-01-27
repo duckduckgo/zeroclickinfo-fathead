786bbde9286d34f91485bdb2e8da7c46a2b28699
aegis <runningvoid@protonmail.com>
2017-01-27 12:19:23 +0100

    Added -f option to sort in parse.sh

--- lib/fathead/java/parse.sh
+++ lib/fathead/java/parse.sh
@@ -4,6 +4,6 @@
 python3 parse.py
 
 if [[ -e "output.txt" && -e "cover/methods.txt" ]]; then
-    LC_ALL=C sort -u output.txt -o output.txt
-    LC_ALL=C sort -u cover/methods.txt -o cover/methods.txt
+    LC_ALL=C sort -f -u output.txt -o output.txt
+    LC_ALL=C sort -f -u cover/methods.txt -o cover/methods.txt
 fi
