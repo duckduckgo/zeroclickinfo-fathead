92327ae2d4376e58a8a32274f676831ccfd2b085
Harrison Chienjo <hhathersage@gmail.com>
2017-02-01 18:45:04 +0300

    Download api page.

--- lib/fathead/python_requests/fetch.sh
+++ lib/fathead/python_requests/fetch.sh
@@ -6,3 +6,4 @@ cd download
 wget http://docs.python-requests.org/en/master/user/quickstart/
 wget -O advanced.html  http://docs.python-requests.org/en/master/user/advanced/
 wget -O authentication.html http://docs.python-requests.org/en/master/user/authentication/
+wget -O api.html http://docs.python-requests.org/en/master/api/
