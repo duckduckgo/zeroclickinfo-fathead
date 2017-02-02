608d8b33d190dcbe744cafe88be26e4ff26f70a0
Robert Nilsson <robert.nilsson85@gmail.com>
2017-02-02 13:11:11 +0000

    Really sort on first field.

--- lib/fathead/react_native/parse.sh
+++ lib/fathead/react_native/parse.sh
@@ -4,7 +4,7 @@ LC_ALL=C
 export PYTHONIOENCODING=utf8
 python3 parse.py
 
-LC_ALL=C sort -u output.txt -o output.txt
+LC_ALL=C sort -u -t$'\t' -k1,1 output.txt -o output.txt
 
 cat output.txt | ./output_statistics.awk
 
