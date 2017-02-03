49964faaa53d61a9444a072f0268c337474590a5
Harrison Chienjo <hhathersage@gmail.com>
2017-02-03 14:10:15 +0300

    Create permalink to article.

--- lib/fathead/python_requests/parse.py
+++ lib/fathead/python_requests/parse.py
@@ -55,10 +55,12 @@ def parse_dl(dl, page_url):
     dt = dl.find('dt')
     func_with_params = dt.text.replace('¶', '').replace('[source]', '')
     func_without_params = dt.get('id')
+    permalink = urljoin(page_url, '#{}'.format(func_without_params))
     module_func = ' '.join(func_without_params.split('.'))
     print(func_with_params)
     print(func_without_params)
     print(module_func)
+    print(permalink)
 
 
 def parse_h2(h2_parent, page_url):
