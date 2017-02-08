219aa42c1b38631145828799f90b59df9a27ad1b
Rahul Arora <rahulxxarora@gmail.com>
2017-02-08 08:38:03 +0000

    Final  changes

--- lib/fathead/electron_api/parse.py
+++ lib/fathead/electron_api/parse.py
@@ -15,17 +15,40 @@ RESULT_URLS = []
 
 
 # Print the content to the output file
-def print_article_line(title, content, url):
+def print_article_line(title, content, url, extras):
 
+    _last = url.split('/')[-1].replace('-', ' ')
+    flag = 0
+    if _last != title:
+        try:
+            if extras[0] == 'event':
+                url = url + '/#event-' + extras[1]
+                flag = 1
+        except:
+            pass
+        if flag == 0:
+            _platforms = title.split()[1:]
+            _title = title.split()[0]
+            url = url + '/#' + _title.lower().replace('.', '').strip()
+            if len(extras) > 0:
+                url = url + extras[0].lower().strip()
+            for i in range(1, len(extras)):
+                url = url + '-' + extras[i].lower().strip()
+            for i in range(len(_platforms)):
+                url = url + '-' + _platforms[i].lower().strip()
     _platform = ''
     content = content.replace('\n', '\\n').replace('\t', '  ')
     if 'Windows' in title:
         _platform = 'Windows'
     if 'macOS' in title:
         _platform = _platform + ' macOS'
+    if 'Linux' in title:
+        _platform = _platform + ' Linux'
     if _platform:
         content = '<span class="prog__sub>Platform:</span><p>'+_platform+'</p>'
-    title = title.replace('Windows', '').replace('macOS', '')
+    title = title.replace('Windows', '')
+    title = title.replace('macOS', '')
+    title = title.replace('Linux', '')
     abstract = '<section class="prog__container">' + content + '</section>'
     abstract = ''.join([i if ord(i) < 128 else ' ' for i in abstract])
 
@@ -51,9 +74,12 @@ def print_article_line(title, content, url):
 # Extracts content of all the pages
 def get_func_desc():
 
+    _new_ul = '<ul class="prog__ul">'
+
     for url in RESULT_URLS:
         title = url.split('/')[-1].lower().replace('-', ' ')
         content = ""
+        extras = []
 
         page = requests.get(url)
         src = page.text
@@ -68,18 +94,18 @@ def get_func_desc():
             if info.name == 'p':
                 content = content + re.sub(r'\<a.*\"\>|\<\/a\>', '', str(info))
             elif info.name == 'ul':
-                content = content + re.sub(r'\<a.*\"\>|\<\/a\>', '', str(info))
-                .replace('<ul>', '<ul class="prog__ul">')
+                content = content + re.sub(r'\<a.*\"\>|\<\/a\>', '',
+                                           str(info)).replace('<ul>', _new_ul)
             elif info.name == 'h3':
-                content = content + '<span class=prog__sub">' +
-                info.text + '</span>'
+                content = content + '<span class=prog__sub">' + info.text \
+                          + '</span>'
             elif info.name == 'div':
                 if 'highlighter-rouge' in info['class']:
                     content = content + '<pre><code>' + \
                               info.text + '</code></pre>'
             info = info.find_next()
 
-        print_article_line(title, content, url)  # Print content to the file
+        print_article_line(title, content, url, extras)  # Print content
 
         # Look for methods and class in the page
         idx = src.find('Methods')
@@ -96,20 +122,30 @@ def get_func_desc():
                     first_time = 1
                 else:
                     if title[:8] != 'Instance':
+                        try:
+                            _extras = re.findall(r'\b[^()]+\((.*)\)', title)
+                            _extras = _extras[0].split(',')
+                            for _temp in _extras:
+                                extras.append(re.sub(r'[\[\]\.]', '', _temp))
+                        except:
+                            pass
                         title = re.sub(r'\([^)]*\)', '', title)
                         if 'Event' in title:
+                            extras.append('event')
                             title = title.replace('Event: ', '')
-                            .replace('‘', '')
+                            title = title.replace('‘', '').replace('’', '')
+                            extras.append(title)
                             title = url.split('/')[-1].lower()
-                            .replace('-', ' ') + ' ' + title
-                        print_article_line(title, content, url)
+                            title = title.replace('-', ' ') + ' ' + title
+                        print_article_line(title, content, url, extras)
+                        extras = []
                     content = ""
                 title = info.text
             elif info.name == 'p' and first_time == 1:
                 content = content + re.sub(r'\<a.*\"\>|\<\/a\>', '', str(info))
             elif info.name == 'ul' and first_time == 1:
-                content = content + re.sub(r'\<a.*\"\>|\<\/a\>', '', str(info))
-                .replace('<ul>', '<ul class="prog__ul">')
+                content = content + re.sub(r'\<a.*\"\>|\<\/a\>', '',
+                                           str(info)).replace('<ul>', _new_ul)
             elif info.name == 'div' and first_time == 1:
                 if 'highlighter-rouge' in info['class']:
                     content = content + '<pre><code>' + \
