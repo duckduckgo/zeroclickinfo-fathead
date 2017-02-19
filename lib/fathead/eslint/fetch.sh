b9e6bab98ec4cdae5884e2f53557bc0f9d8a226a
zodiac 403 <zodiac403@gmx.de>
2017-02-19 11:28:53 +0000

    Fetch: Clone repo containing detailed input files.

--- lib/fathead/eslint/fetch.sh
+++ lib/fathead/eslint/fetch.sh
@@ -4,6 +4,11 @@
 rm -R download
 mkdir -p download
 
-# download
+# summary
 mkdir -p download/summary
 curl https://raw.githubusercontent.com/eslint/eslint.github.io/master/_data/rules.yml -o ./download/summary/rules.yml
+
+# full repo
+## curl https://github.com/eslint/eslint.github.io/archive/master.zip -o ./download/repo.zip
+## unzip ./download/repo.zip -d ./download/repo/
+git clone https://github.com/eslint/eslint.github.io download/repo/
