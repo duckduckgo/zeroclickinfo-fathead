4004405176dfc3bf9d6a3ac26922fb4d68760fba
Aditya Tandon <adityatandon007@gmail.com>
2017-02-21 10:52:39 +0000

    adding comments and some cleanup

--- lib/fathead/pytest/parse.py
+++ lib/fathead/pytest/parse.py
@@ -18,7 +18,7 @@ url_fmt = 'http://doc.pytest.org/en/latest/builtin.html%s'
 
 def clean_output(output):
     """Remove newlines from abstract."""
-    return re.sub('\r?\n+', '', output)
+    return re.sub('\r?\n+', ' ', output)
 
 
 def clean_code(code):
@@ -27,13 +27,17 @@ def clean_code(code):
 
 
 def generate_api_example(api_examples):
+    
     """Generate a new output entry."""
+    
     for api_example in api_examples:
 
         title = api_example.dt.get('id')
         link = api_example.find("a", {'class': 'headerlink'}).extract()
         url = url_fmt % link['href']
+       
         # if the example contains code, add it to the abstract
+        
         if api_example.pre:
             code = clean_code(api_example.pre.text)
             code = '<pre><code>{}</code></pre>'.format(code)
@@ -43,23 +47,36 @@ def generate_api_example(api_examples):
         abstract = clean_output(api_example.dd.p.text)
         abstract = abstract_fmt.format(abstract, code)
        
+        """ Yielding output entry """
+        
         yield [title, 'A', '', '', '', '', '', '', '', '', '', abstract, url]
   
 def main():
     with open(INFILE, 'r', encoding='utf-8') as f, open(OUTFILE, 'w', encoding='utf-8') as o:
+        
+        """ Creating soup for input file """
+        
         soup = BeautifulSoup(f, 'html.parser')
+        
+        """ Pasing Functions """
+        
         api_functions = soup.select('dl[class="function"]')
-        #print(api_functions)
+        
         for api_function in generate_api_example(api_functions):
             output = '\t'.join(api_function)
             o.write(output + '\n')
+            
+        """ Parsing Attributes """
+        
         api_attributes = soup.select('dl[class="attribute"]')
-        #print(api_attributes)
+       
         for api_attribute in generate_api_example(api_attributes):
             output = '\t'.join(api_attribute)
             o.write(output + '\n')
+        
+        """ Parsing Classes """
+        
         api_classes = soup.select('dl[class="class"]')
-        #print(api_classes)
         for api_class in generate_api_example(api_classes):
             output = '\t'.join(api_class)
             o.write(output + '\n')
