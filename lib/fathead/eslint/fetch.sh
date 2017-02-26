3a097485887ffced32440eed9222d3233a3a8446
zodiac 403 <zodiac403@gmx.de>
2017-02-26 16:00:40 +0000

    Code: 'fetch.sh' - Avoid prompting for deletion. - Remove comments.

--- lib/fathead/eslint/fetch.sh
+++ lib/fathead/eslint/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # clean up
-rm -R download
+rm -fR download
 mkdir -p download
 
 # summary
@@ -9,6 +9,4 @@ mkdir -p download/summary
 curl https://raw.githubusercontent.com/eslint/eslint.github.io/master/_data/rules.yml -o ./download/summary/rules.yml
 
 # full repo
-## curl https://github.com/eslint/eslint.github.io/archive/master.zip -o ./download/repo.zip
-## unzip ./download/repo.zip -d ./download/repo/
 git clone https://github.com/eslint/eslint.github.io download/repo/
