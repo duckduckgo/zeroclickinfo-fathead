ad4d1833da8ff3227587ec851205d3877a6b8e7b
Kush Jain <kushj2008@gmail.com>
2017-02-28 04:20:19 +0530

    removed accidentally added files

--- lib/fathead/gruntjs/fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p "./download"
-
-wget --continue --directory-prefix="./download" --html-extension --no-parent --recursive --no-directories "http://gruntjs.com/api/grunt"
-
-rm -rf "./download/robots.txt" "./download/exit-codes.html" "./download/inside-tasks.html" "./download/grunt.html"
--- lib/fathead/gruntjs/parse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-node ./parse.js
-LC_ALL=C sort output.txt -o output.txt
