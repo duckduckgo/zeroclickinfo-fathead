03449a94325cd6741887a5527c62f0f1823209be
Billy Barbaro <billy@thebarbaros.com>
2017-03-05 15:27:04 -0500

    A few changes to the parsing of summary. Trying to use python3.

--- lib/fathead/android_api_docs/parse.py
+++ lib/fathead/android_api_docs/parse.py
@@ -1,13 +1,8 @@
-#!/usr/bin env python
-# encoding=utf8
-import sys
+#!/usr/bin/env
+
 import re
 from bs4 import BeautifulSoup
 from os import walk
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-#ascii error fix: http://stackoverflow.com/questions/21129020/how-to-fix-unicodedecodeerror-ascii-codec-cant-decode-byte
 
 
 BASE_URL = 'https://developer.android.com/'
@@ -19,24 +14,29 @@ def remove_anchor_tags(method_details):
 
 
 def build_summary_article(soup, file_path, file_name):
-    # If soup can't find the proper h1, use the filename as title without the .html
-    title = soup.find('h1').contents[0].encode('utf-8')
+    title_list = soup.select('#jd-content > h1')
+    if len(title_list) != 1:
+        raise Exception('Summary title not found for file {}'.format(file_name))
+    title = title_list[0].text
 
     file_path = file_path[11:]
     url = BASE_URL + file_path
 
-    abstract = soup.find('p').contents[0].encode('utf-8')
+    abstract_paragraphs = soup.select('#jd-content > p')
+
+    if len(abstract_paragraphs) > 0:  # For brevity's sake, just take the first paragraph of the package's summary
+        abstract = abstract_paragraphs[0].text
+    else:  # There's no abstract for us to provide. So don't include this article
+        return None
 
     # Remove carriage returns and extra whitespace
-    abstract = ''.join(abstract.splitlines())
-    abstract = ' '.join(abstract.split())
+    abstract = re.sub(r'\s+', ' ', abstract)
 
     # Use DuckDuckHack's recommended code snippet wraps
     abstract = abstract.replace("<code>", "<pre><code>")
     abstract = abstract.replace("</code>", "</code></pre>")
 
-    abstract = '<section class="prog__container">%s</section>' % abstract
-    abstract = abstract.encode('utf-8')
+    abstract = '<section class="prog__container">{}</section>'.format(abstract)
 
     print('Title %s ' % title)
     print('URL %s' % url)
@@ -61,79 +61,63 @@ def build_summary_article(soup, file_path, file_name):
 
 
 def build_article(soup, file_path, file_name):
-    # If soup can't find the proper h1, use the filename as title without the .html
-    try:
-        title = soup.find('h1').contents[0].encode('utf-8')
-    except:
-        title = str(file_name).partition(".html")[0]
+    title = str(file_name).partition(".html")[0]
 
     file_path = file_path[11:]
     url = BASE_URL + file_path
 
     # On the off chance soup can't find the desired snippets, use fallback
-    try:
-        # First line of expample code listed
-        first_code = soup.find('code')
-        remove_anchor_tags(first_code)
-        # Second line of example code in doc
-        second_code = first_code.find_next_sibling().find('code')
-        remove_anchor_tags(second_code)
+    # First line of expample code listed
+    first_code = soup.find('code')
+    remove_anchor_tags(first_code)
+    # Second line of example code in doc
+    second_code = first_code.find_next_sibling().find('code')
+    remove_anchor_tags(second_code)
 
-        # Credit for crlf removal: http://stackoverflow.com/questions/16149649/remove-carriage-return-in-python
-        # Credit for the sporadic whitespace removal: http://stackoverflow.com/questions/2077897/substitute-multiple-whitespace-with-single-whitespace-in-python
-        first_code = first_code.contents[0].encode('utf-8')
-        first_code = ''.join(first_code.splitlines())
-        first_code = ' '.join(first_code.split())
+    # Credit for crlf removal: http://stackoverflow.com/questions/16149649/remove-carriage-return-in-python
+    # Credit for the sporadic whitespace removal: http://stackoverflow.com/questions/2077897/substitute-multiple-whitespace-with-single-whitespace-in-python
+    first_code = first_code.contents[0].encode('utf-8')
+    first_code = ''.join(first_code.splitlines())
+    first_code = ' '.join(first_code.split())
 
-        # Credit for full inner html: http://stackoverflow.com/questions/8112922/beautifulsoup-innerhtml
-        second_code = second_code.decode_contents(formatter="html")
-        second_code = second_code.encode('utf-8')
-        # Remove carriage returns and extra whitespace
-        second_code = ''.join(second_code.splitlines())
-        second_code = ' '.join(second_code.split())
+    # Credit for full inner html: http://stackoverflow.com/questions/8112922/beautifulsoup-innerhtml
+    second_code = second_code.decode_contents(formatter="html")
+    second_code = second_code.encode('utf-8')
+    # Remove carriage returns and extra whitespace
+    second_code = ''.join(second_code.splitlines())
+    second_code = ' '.join(second_code.split())
 
-        class_description = soup.find("hr").find_next("p")
-        remove_anchor_tags(class_description)
-        class_description = class_description.prettify()
-        class_description = class_description.encode('utf-8')
+    class_description = soup.find("hr").find_next("p")
+    remove_anchor_tags(class_description)
+    class_description = class_description.prettify()
+    class_description = class_description.encode('utf-8')
 
-        # Google unfortunately didn't close quite a few p tags, so we need to grab the description contents this way
-        # Credit for partition: http://stackoverflow.com/questions/14801057/python-splitting-to-the-newline-character
-        try:
-            class_description = class_description.partition("<h2")[0]
-        except:
-            print("No h2")
-        try:
-            class_description = class_description.partition("<h3")[0]
-        except:
-            print("No 33")
-        try:
-            class_description = class_description.partition("<div")[0]
-        except:
-            print("No div")
-        # Add missing closing p tags where needed
-        try:
-            class_description = class_description.partition("</p>")[0]
-            class_description += "</p>"
-        except:
-            print("No closing </p>")
+    # Google unfortunately didn't close quite a few p tags, so we need to grab the description contents this way
+    # Credit for partition: http://stackoverflow.com/questions/14801057/python-splitting-to-the-newline-character
+    class_description = class_description.partition("<h2")[0]
 
-        # Remove carriage returns and extra whitespace
-        class_description = ''.join(class_description.splitlines())
-        class_description = ' '.join(class_description.split())
+    class_description = class_description.partition("<h3")[0]
 
-        # Use DuckDuckHack's recommended code snippet wraps
-        class_description = class_description.replace("<code>", "<pre><code>")
-        class_description = class_description.replace("</code>", "</code></pre>")
+    class_description = class_description.partition("<div")[0]
 
-        # Build the abstract from the description and example code usage
-        abstract = ""
-        if re.search(r'<p>\s*</p>', class_description) is None:
-            abstract += class_description.encode('utf-8')
-        abstract += "<pre><code>%s</code></pre>" % ((first_code + "\\n" + second_code).encode('utf-8'))
-        abstract = '<section class="prog__container">%s</section>' % abstract
-    except:
-        abstract = "For more information, see " + url
+    # Add missing closing p tags where needed
+    class_description = class_description.partition("</p>")[0]
+    class_description += "</p>"
+
+    # Remove carriage returns and extra whitespace
+    class_description = ''.join(class_description.splitlines())
+    class_description = ' '.join(class_description.split())
+
+    # Use DuckDuckHack's recommended code snippet wraps
+    class_description = class_description.replace("<code>", "<pre><code>")
+    class_description = class_description.replace("</code>", "</code></pre>")
+
+    # Build the abstract from the description and example code usage
+    abstract = ""
+    if re.search(r'<p>\s*</p>', class_description) is None:
+        abstract += class_description.encode('utf-8')
+    abstract += "<pre><code>%s</code></pre>" % ((first_code + "\\n" + second_code).encode('utf-8'))
+    abstract = '<section class="prog__container">%s</section>' % abstract
 
     abstract = abstract.encode('utf-8')
 
@@ -172,11 +156,12 @@ if __name__ == '__main__':
                 if fname == "package-summary.html":
                     print("\tThe summary file:" + fname)
                     data = build_summary_article(soup, filePath, fname)
-                    data = '\t'.join(data)
-                    fp.write('{}\n'.format(data))
+                    if data is not None:
+                        data = '\t'.join(data)
+                        fp.write('{}\n'.format(data))
                 else:
                     # Build regular article with code highlighting
                     print('\tThe file:%s' % fname)
-                    data = build_article(soup, filePath, fname)
-                    data = '\t'.join(data)
-                    fp.write('{}\n'.format(data))
+                    # data = build_article(soup, filePath, fname)
+                    # data = '\t'.join(data)
+                    # fp.write('{}\n'.format(data))
