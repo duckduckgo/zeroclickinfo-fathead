2e8fa25ad2cc23941f65da9bd7eb838933113551
Povilas Kanapickas <povilas@radix.lt>
2017-03-26 16:16:47 +0300

    cppreference_doc: Work around a DDG bug that hides empty pairs of parentheses

--- lib/fathead/cppreference_doc/parse.sh
+++ lib/fathead/cppreference_doc/parse.sh
@@ -3,4 +3,9 @@
 # we can use glob since the archive contains a single directory
 pushd download/*/ > /dev/null
 python3 index2ddg.py index-functions-cpp.xml ../../output.txt --split_code_snippets
+
+# work around a DDG bug that strips empty parentheses
+# note that we can't just match '()', as that would change identifier names
+sed -e 's/()\([^\t]\)/(void)\1/g' -i ../../output.txt
+
 popd > /dev/null
