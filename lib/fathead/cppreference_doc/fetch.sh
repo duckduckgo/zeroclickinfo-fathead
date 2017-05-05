b2835be5bc859d1065a22a85cb1793200569de09
Povilas Kanapickas <povilas@radix.lt>
2017-05-05 22:22:35 +0300

    C++ fathead: Use local scripts for parsing

--- lib/fathead/cppreference_doc/fetch.sh
+++ lib/fathead/cppreference_doc/fetch.sh
@@ -7,4 +7,5 @@ wget upload.cppreference.com/w/Cppreference:DDG-link?action=raw -O cppreference-
 
 wget -i cppreference-link -O cppreference-doc-ddg.tar.gz
 tar -xzf cppreference-doc-ddg.tar.gz
+mv cppreference-doc-20* cppreference-doc
 popd > /dev/null
