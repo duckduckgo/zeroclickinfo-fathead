9651e5e410b2da4e53837a3c2b83da901039704c
Sourav Singh <souravsingh@users.noreply.github.com>
2017-07-23 01:08:26 +0530

    Make updates to parser

--- lib/fathead/pandas/parse.py
+++ lib/fathead/pandas/parse.py
@@ -25,3 +25,30 @@ class Python_Data(object):
         """
         with open(self.FILE, 'r') as data_file:
             self.HTML = data_file.read()
+    
+    def get_raw_data(self):
+        """
+        Returns: The raw HTML that was loaded.
+        """
+        return self.HTML
+
+    def get_file(self):
+        """
+        Returns: The file path of the file being used.
+        """
+        return self.FILE
+    
+    
+class PythonDataParser(object):
+    """
+    Object responsible for parsing the raw HTML that contains Python data
+    """
+    def __init__(self, data_object):
+        """
+        Given raw data, get the relevant sections
+        Args:
+            raw_data: HTML data
+        """
+        self.parsed_data = []
+        self.soup = BeautifulSoup(data_object.get_raw_data(), 'html.parser')
+
