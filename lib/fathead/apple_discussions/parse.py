233682d5819862d66aa090a79efe51d7d80f32d3
PJ Hampton <pjhampton@protonmail.com>
2017-08-09 21:34:33 +0100

    Update Parser.

--- lib/fathead/apple_discussions/parse.py
+++ lib/fathead/apple_discussions/parse.py
@@ -12,6 +12,7 @@ __MODULE__ = "apple_discussions"
 __AUTHOR__ = "DuckDuckGo [https://duckduckgo.com]"
 __SOURCE__ = "https://discussions.apple.com/"
 __OUTPUT__ = "output.txt"
+__DEBUG__ = False
 
 import argparse
 import glob
@@ -39,6 +40,11 @@ OUTPUT = """\
 {url}
 """.replace("\n", "")
 
+FHTEMPLATE = """\
+<p><b>Answered by {username} ({date})</b></p>
+{information}
+""".replace("\n", "")
+
 def parse_file(filename):
     """
     The pipeline to process the files
@@ -50,6 +56,9 @@ def parse_file(filename):
             if parsed_document != False:
                 output = format_output(parsed_document)
                 write_to_output(output)
+            # If we can't parsed and __DEBUG__ is switched on we log to "errors.txt"
+            elif __DEBUG__ == True and parsed_document == False:
+                write_to_output(filename, output="errors.txt")
     except:
         pass # parser can't parse
 
@@ -67,21 +76,57 @@ def parse_html(doc, url):
 
     # Gets the title
     title = soup.h1.text
-    parsed_doc["title"] = title.strip().replace("Q: ", "")
+    temp_title = title.strip().replace("Q: ", "")
+    parsed_doc["title"] = temp_title[0].capitalize() + temp_title[1:] # Capitalizes first word
     print("Parsing", parsed_doc["title"])
+
+    # Adds category to the title (if we can)
+    try:
+        topic = soup.find("a", {"class": "jive-breadcrumb-last"}).text
+        topic = topic.replace("Using ", "")
+        parsed_doc["title"] += " ({topic})".format(topic=topic)
+    except:
+        pass
 
     # Get's the most 'Recommended Answer'
     if soup.find("div", {"class": "recommended-answers"}):
-        parsed_doc["body"] = soup.find("div", {"class", "recommended-answers"}).find("section").find("div", {"class": "jive-rendered-content"}).text
-    else:
-        return False
 
-    # Some last moment validation
-    if "dp-highlighter" in parsed_doc["body"]: # contains code
-        return False
-    if "blockquote" in parsed_doc["body"]:
-        return False
-    if len(parsed_doc["body"].split(" ")) > 400:
+        for p in soup.findAll('p'):
+            if p.string:
+                p.string.replace_with(p.string.strip())
+
+        username = soup.find("div", {"class", "recommended-answers"}).find("a", {"class": "username"}).text
+        username = username.strip()
+
+        posted = soup.find("div", {"class", "recommended-answers"}).find("p", {"class": "meta-posted"}).text
+        posted = posted.strip().replace("Posted on ", "")
+        posted = posted.split(" ")
+        posted = posted[0] + " " + posted[1] + " " + posted[2]
+
+        content = soup.find("div", {"class", "recommended-answers"}).find("section").find("div", {"class": "jive-rendered-content"})
+
+        for tags in content.findAll(True):
+            tags.attrs = {}
+
+        # Does some regex replacements that the parser just won't hit with grace
+        contents = FHTEMPLATE.format(information=str(content), username=username, date=posted)
+        contents = re.sub(re.compile("<p></p>"), "", contents)
+        contents = re.sub(re.compile("<div ((.|\n)+?)>"), "", contents)
+        contents = re.sub(re.compile("</div>"), "", contents)
+        contents = re.sub(re.compile("<a>"), "", contents)
+        contents = re.sub(re.compile("</a>"), "", contents)
+        parsed_doc["body"] = contents
+
+        # Some last moment validation
+        if "dp-highlighter" in parsed_doc["body"]: # contains code
+            return False
+        if "blockquote" in parsed_doc["body"]:
+            return False
+        if len(parsed_doc["body"].split(" ")) > 500:
+            return False
+        if len(parsed_doc["body"].split(" ")) < 30:
+            return False
+    else:
         return False
 
     return parsed_doc
@@ -107,11 +152,11 @@ def format_output(doc):
 
     return document
 
-def write_to_output(article):
+def write_to_output(article, output=__OUTPUT__):
     """
     Appends the parsed article to the `output.txt` file.
     """
-    with open(__OUTPUT__, 'a', encoding="utf-8") as f:
+    with open(output, 'a', encoding="utf-8") as f:
         f.write(article + "\n")
 
 if __name__ == "__main__":
@@ -127,3 +172,4 @@ if __name__ == "__main__":
 
     pool = Pool(args.processes)
     pool.map(parse_file, files_to_parse)
+
--- lib/fathead/apple_discussions/parse.sh
+++ lib/fathead/apple_discussions/parse.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 date
+rm -rf errors.txt output.txt # new content would be appended otherwise
 python3 parse.py apple_data/
 sort -u -o output.txt output.txt
 echo "fin."
