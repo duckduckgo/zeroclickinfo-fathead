a75e0fa7b7aff5930e0f02bad4fc17c65c90edbb
Philip Kirkbride <phk@2klic.com>
2017-08-21 11:52:56 -0400

    break lines with 2 entries into 2

--- lib/fathead/get_emoji/parse.sh
+++ lib/fathead/get_emoji/parse.sh
@@ -5,3 +5,6 @@ sed -i 's/.*(//' output.txt # Remove columns not needed
 sed -i 's|[(),]||g' output.txt # Remove brackets around emoji
 sed -i 's/\(.*[^ ]\)[ ]*\(.*\)/\2 \1/' output.txt # Move first column to last
 sed -i '/^$/d' output.txt # remove blank lines
+
+# break lines with 2 entries into 2 lines
+sed -i '/.\.\./ s/\(.\)\.\.\(.\)\s*\(.*\)\.\./\1\t\3\n \2\t/' output.txt
