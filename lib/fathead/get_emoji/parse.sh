3aebee6618477a11ac33e35ba5e32171955f3ca2
Philip Kirkbride <phk@2klic.com>
2017-08-21 13:43:19 -0400

    improve sed function breaking into 2 lines:

--- lib/fathead/get_emoji/parse.sh
+++ lib/fathead/get_emoji/parse.sh
@@ -4,9 +4,10 @@ sed -i '/^#/ d' output.txt                        # Remove comments
 sed -i 's/.*(//' output.txt                       # Remove columns not needed
 sed -i 's|[(),]||g' output.txt                    # Remove brackets
 sed -i 's/\(.*[^ ]\)[ ]*\(.*\)/\2 \1/' output.txt # Move first column to last
-sed -i '/^$/d' output.txt                         # remove blank lines
+sed -i '/^$/d' output.txt                         # Remove blank lines
 
-# break lines with 2 entries into 2 lines
-sed -i '/.\.\./ s/\(.\)\.\.\(.\)\s*\(.*\)\.\./\1\t\3\n \2\t/' output.txt
+# Break lines with 2 entries into 2 lines
+sed -i 's/\.\.\([^ ]*\) *\(.*\)\.\./\t\2\n \1\t/' output.txt
 
-sed -i "s/[[:space:]]\+/ /g" output.txt           # format spacing
+
+sed -i "s/[[:space:]]\+/ /g" output.txt           # Format spacing
