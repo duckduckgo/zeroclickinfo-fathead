af5fc71f0bc73c43a0ab287cbd7fae42e4ee0fc1
Philip Kirkbride <phk@2klic.com>
2017-08-21 11:58:39 -0400

    format spacing and comments

--- lib/fathead/get_emoji/parse.sh
+++ lib/fathead/get_emoji/parse.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-sed -i '/^#/ d' output.txt # Remove comments
-sed -i 's/.*(//' output.txt # Remove columns not needed
-sed -i 's|[(),]||g' output.txt # Remove brackets around emoji
+sed -i '/^#/ d' output.txt                        # Remove comments
+sed -i 's/.*(//' output.txt                       # Remove columns not needed
+sed -i 's|[(),]||g' output.txt                    # Remove brackets
 sed -i 's/\(.*[^ ]\)[ ]*\(.*\)/\2 \1/' output.txt # Move first column to last
-sed -i '/^$/d' output.txt # remove blank lines
+sed -i '/^$/d' output.txt                         # remove blank lines
 
 # break lines with 2 entries into 2 lines
 sed -i '/.\.\./ s/\(.\)\.\.\(.\)\s*\(.*\)\.\./\1\t\3\n \2\t/' output.txt
+
+sed -i "s/[[:space:]]\+/ /g" output.txt           # format spacing
